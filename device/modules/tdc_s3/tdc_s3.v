220cd4dde43c05428eaac04cf2dd5dfaefd2ea1d
Jens Janssen <janssen@physik.uni-bonn.de>
2014-02-10 15:32:47 +0000

    ENH: TDC_IN added feed through (TDC_OUT) ENH: added TDC arm BUG: fixed clock domain for FSM signals MAINT: cleanup

--- device/modules/tdc_s3/tdc_s3.v
+++ device/modules/tdc_s3/tdc_s3.v
@@ -9,13 +9,13 @@
  *  $Author::                    $:
  *  $Date::                      $:
  */
- 
+
 module tdc_s3
 #(
     parameter BASEADDR = 16'h0000,
     parameter HIGHADDR = 16'h0000,
-    
-    parameter IDENTYFIER = 4'b0001
+
+    parameter DATA_IDENTIFIER = 4'b0100
 )(
     input BUS_CLK,
     input [15:0] BUS_ADD,
@@ -23,58 +23,63 @@ module tdc_s3
     input BUS_RST,
     input BUS_WR,
     input BUS_RD,
-    
+
     input CLK320,
     input CLK160,
     input CLK40,
     input TDC_IN,
+    output TDC_OUT,
 
     input FIFO_READ,
     output FIFO_EMPTY,
-    output [31:0] FIFO_DATA
-    
-); 
+    output [31:0] FIFO_DATA,
 
-    wire IP_RD, IP_WR;
-    wire [15:0] IP_ADD;
-    wire [7:0] IP_DATA_IN;
-    wire [7:0] IP_DATA_OUT;
-    
-    bus_to_ip #( .BASEADDR(BASEADDR), .HIGHADDR(HIGHADDR) ) i_bus_to_ip
-    (
-        .BUS_RD(BUS_RD),
-        .BUS_WR(BUS_WR),
-        .BUS_ADD(BUS_ADD),
-        .BUS_DATA(BUS_DATA),
-    
-        .IP_RD(IP_RD),
-        .IP_WR(IP_WR),
-        .IP_ADD(IP_ADD),
-        .IP_DATA_IN(IP_DATA_IN),
-        .IP_DATA_OUT(IP_DATA_OUT)
-    );
-    
-    tdc_s3_core 
-    #(
-        .IDENTYFIER(IDENTYFIER)
-    ) i_tdc_s3_core
-    (
-        .BUS_CLK(BUS_CLK),                     
-        .BUS_RST(BUS_RST),                  
-        .BUS_ADD(IP_ADD),                    
-        .BUS_DATA_IN(IP_DATA_IN),                    
-        .BUS_RD(IP_RD),                    
-        .BUS_WR(IP_WR),                    
-        .BUS_DATA_OUT(IP_DATA_OUT),
-          
-        .CLK320(CLK320),
-        .CLK160(CLK160),
-        .CLK40(CLK40),
-        .TDC_IN(TDC_IN),
-    
-        .FIFO_READ(FIFO_READ),
-        .FIFO_EMPTY(FIFO_EMPTY),
-        .FIFO_DATA(FIFO_DATA)
-    );
+    input ARM_TDC
+);
+
+wire IP_RD, IP_WR;
+wire [15:0] IP_ADD;
+wire [7:0] IP_DATA_IN;
+wire [7:0] IP_DATA_OUT;
+
+bus_to_ip #( .BASEADDR(BASEADDR), .HIGHADDR(HIGHADDR) ) i_bus_to_ip
+(
+    .BUS_RD(BUS_RD),
+    .BUS_WR(BUS_WR),
+    .BUS_ADD(BUS_ADD),
+    .BUS_DATA(BUS_DATA),
+
+    .IP_RD(IP_RD),
+    .IP_WR(IP_WR),
+    .IP_ADD(IP_ADD),
+    .IP_DATA_IN(IP_DATA_IN),
+    .IP_DATA_OUT(IP_DATA_OUT)
+);
+
+tdc_s3_core 
+#(
+    .DATA_IDENTIFIER(DATA_IDENTIFIER)
+) i_tdc_s3_core
+(
+    .BUS_CLK(BUS_CLK),
+    .BUS_RST(BUS_RST),
+    .BUS_ADD(IP_ADD),
+    .BUS_DATA_IN(IP_DATA_IN),
+    .BUS_RD(IP_RD),
+    .BUS_WR(IP_WR),
+    .BUS_DATA_OUT(IP_DATA_OUT),
+
+    .CLK320(CLK320),
+    .CLK160(CLK160),
+    .CLK40(CLK40),
+    .TDC_IN(TDC_IN),
+    .TDC_OUT(TDC_OUT),
+
+    .FIFO_READ(FIFO_READ),
+    .FIFO_EMPTY(FIFO_EMPTY),
+    .FIFO_DATA(FIFO_DATA),
+
+    .ARM_TDC(ARM_TDC)
+);
 
 endmodule
--- device/modules/tdc_s3/tdc_s3_core.v
+++ device/modules/tdc_s3/tdc_s3_core.v
@@ -12,12 +12,13 @@
  
 module tdc_s3_core
 #(
-    parameter IDENTYFIER = 4'b0001
+    parameter DATA_IDENTIFIER = 4'b0100
 )(
     input CLK320,
     input CLK160,
     input CLK40,
     input TDC_IN,
+    output TDC_OUT,
 
     input FIFO_READ,
     output FIFO_EMPTY,
@@ -29,10 +30,12 @@ module tdc_s3_core
     output reg [7:0] BUS_DATA_OUT,
     input BUS_RST,
     input BUS_WR,
-    input BUS_RD
-); 
+    input BUS_RD,
 
-//output format #ID (as parameter IDENTYFIER + 16 event counter + 12 TDC bit data) 
+    input ARM_TDC // assuming slower than 48MHz
+);
+
+//output format #ID (4 bit DATA_IDENTIFIER (parameter) + 16 bit event counter + 12 bit TDC data) 
 
 wire SOFT_RST;
 assign SOFT_RST = (BUS_ADD==0 && BUS_WR);
@@ -40,41 +43,51 @@ assign SOFT_RST = (BUS_ADD==0 && BUS_WR);
 wire RST;
 assign RST = BUS_RST | SOFT_RST; 
 
-reg CONF_EN; //ENABLE BUS_ADD==2 BIT==0
-reg [7:0] LOST_DATA_CNT; //BUS_ADD==3
+
+reg [7:0] status_regs[1:0];
+
+wire CONF_EN; //ENABLE BUS_ADD==1 BIT==0
+assign CONF_EN = status_regs[1][0];
+wire CONF_EN_ARM_TDC; //ENABLE BUS_ADD==1 BIT==1
+assign CONF_EN_ARM_TDC = status_regs[1][1];
+
+reg [7:0] LOST_DATA_CNT; //BUS_ADD==2
 
 always @(posedge BUS_CLK) begin
     if(RST) begin
-        CONF_EN <= 0;
+        status_regs[0] <= 8'b0;
+        status_regs[1] <= 8'b0;
     end
-    else if(BUS_WR) begin
-        if(BUS_ADD == 2)
-            CONF_EN <= BUS_DATA_IN[0];
+    else if(BUS_WR && BUS_ADD < 2)
+    begin
+        status_regs[BUS_ADD[0]] <= BUS_DATA_IN;
     end
 end
 
-
-
 always @(posedge BUS_CLK) begin
     if(BUS_RD) begin
-        if(BUS_ADD == 2)
-            BUS_DATA_OUT <= {7'b0, CONF_EN};
-        else if(BUS_ADD == 3)
+        if (BUS_ADD == 0)
+            BUS_DATA_OUT <= status_regs[0];
+        else if(BUS_ADD == 1)
+            BUS_DATA_OUT <= status_regs[1];
+        else if(BUS_ADD == 2)
             BUS_DATA_OUT <= LOST_DATA_CNT;
     end
 end
 
-////
+// 320MHz clock domain
+// *** do not touch code below ***
+
 wire [1:0] DDRQ;
 IFDDRRSE IFDDRRSE_inst (
-.Q0(DDRQ[1]), // Posedge data output
-.Q1(DDRQ[0]), // Negedge data output
-.C0(CLK320), // 0 degree clock input
-.C1(~CLK320), // 180 degree clock input
-.CE(1'b1), // Clock enable input
-.D(TDC_IN), // Data input (connect directly to top-level port)
-.R(1'b0), // Synchronous reset input
-.S(1'b0) // Synchronous preset input
+    .Q0(DDRQ[1]), // Posedge data output
+    .Q1(DDRQ[0]), // Negedge data output
+    .C0(CLK320), // 0 degree clock input
+    .C1(~CLK320), // 180 degree clock input
+    .CE(1'b1), // Clock enable input
+    .D(TDC_IN), // Data input (connect directly to top-level port)
+    .R(1'b0), // Synchronous reset input
+    .S(1'b0) // Synchronous preset input
 );
 
 reg [1:0] DDRQ_DLY;
@@ -86,23 +99,27 @@ reg [3:0] DDRQ_DATA;
 always@(posedge CLK320)
     DDRQ_DATA[3:0] <= {DDRQ_DLY[1:0], DDRQ[1:0]};
 
+// *** do not touch code above ***
+
 reg [3:0] DATA_IN;
 always@(posedge CLK160)
     DATA_IN[3:0] <= {DDRQ_DATA[3:0]};
 
+assign TDC_OUT = |DATA_IN;
+
 reg [15:0] DATA_IN_SR;
 always@(posedge CLK160)
     DATA_IN_SR[15:0] <= {DATA_IN_SR[11:0],DATA_IN[3:0]};
-    
+
 reg [15:0] DATA;
 always@(posedge CLK40)
     DATA <= DATA_IN_SR;
-    
+
 wire ONE_DETECTED;
-assign ONE_DETECTED = |DATA;   
+assign ONE_DETECTED = |DATA;
 
 wire ZERO_DETECTED;
-assign ZERO_DETECTED = |(~DATA);   
+assign ZERO_DETECTED = |(~DATA);
 
 wire RST_SYNC;
 flag_domain_crossing cmd_rst_flag_domain_crossing (
@@ -112,46 +129,97 @@ flag_domain_crossing cmd_rst_flag_domain_crossing (
     .FLAG_OUT_CLK_B(RST_SYNC)
 );
 
+reg [7:0] sync_cnt;
+always@(posedge BUS_CLK) begin
+    if(RST)
+        sync_cnt <= 120;
+    else if(sync_cnt != 100)
+        sync_cnt <= sync_cnt +1;
+end 
 
-reg state, next_state;
-localparam      WAIT  = 0,
-                COUNT = 1;
+wire RST_LONG;
+assign RST_LONG = sync_cnt[7];
+
+
+wire ARM_TDC_BUS_CLK;
+three_stage_synchronizer three_stage_rj45_trigger_synchronizer_bus_clk (
+    .CLK(BUS_CLK),
+    .IN(ARM_TDC),
+    .OUT(ARM_TDC_BUS_CLK)
+);
+
+wire ARM_TDC_CLK40;
+flag_domain_crossing arm_tdc_flag_domain_crossing (
+    .CLK_A(BUS_CLK),
+    .CLK_B(CLK40),
+    .FLAG_IN_CLK_A(ARM_TDC_BUS_CLK),
+    .FLAG_OUT_CLK_B(ARM_TDC_CLK40)
+);
+
+wire CONF_EN_CLK40;
+three_stage_synchronizer conf_en_three_stage_synchronizer_clk40 (
+    .CLK(CLK40),
+    .IN(CONF_EN),
+    .OUT(CONF_EN_CLK40)
+);
+
+wire CONF_EN_ARM_TDC_CLK40;
+three_stage_synchronizer conf_en_arm_three_stage_synchronizer_clk40 (
+    .CLK(CLK40),
+    .IN(CONF_EN_ARM_TDC),
+    .OUT(CONF_EN_ARM_TDC_CLK40)
+);
+
+reg [1:0] state, next_state;
+localparam      IDLE  = 2'b00,
+                ARMED = 2'b01,
+                COUNT = 2'b10;
 
 always @ (posedge CLK40)
     if (RST_SYNC)
-      state <= WAIT;
+      state <= IDLE;
     else
       state <= next_state;
 
-always @ (*) begin 
-    case(state)        
-        WAIT:
-            if (ONE_DETECTED && CONF_EN)
+always @ (state or ONE_DETECTED or ZERO_DETECTED or CONF_EN_CLK40 or CONF_EN_ARM_TDC_CLK40 or ARM_TDC_CLK40) begin
+    case(state)
+        IDLE:
+            if (ONE_DETECTED && CONF_EN_CLK40 && !CONF_EN_ARM_TDC_CLK40)
                 next_state = COUNT;
+            else if (ARM_TDC_CLK40 && CONF_EN_CLK40 && CONF_EN_ARM_TDC_CLK40)
+                next_state = ARMED;
             else
-                next_state = WAIT;
+                next_state = IDLE;
+
+        ARMED:
+            if (ONE_DETECTED)
+                next_state = COUNT;
+            else if (!CONF_EN_CLK40)
+                next_state = IDLE;
+            else
+                next_state = ARMED;
 
         COUNT:
             if (ZERO_DETECTED)
-                next_state = WAIT;
+                next_state = IDLE;
+            else if (!CONF_EN_CLK40)
+                next_state = IDLE;
             else
                 next_state = COUNT;
-        default : next_state = WAIT;
+        default : next_state = IDLE;
     endcase
-end 
-
-
+end
 
 wire FINISH;
-assign FINISH = (state == COUNT && next_state == WAIT);
+assign FINISH = (state == COUNT && next_state == IDLE);
 wire START;
-assign START = (state == WAIT && next_state == COUNT);
+assign START = ((state == IDLE && next_state == COUNT) || (state == ARMED && next_state == COUNT));
 
 integer i;
 reg [4:0] ONES;
 always@(*) begin 
     ONES = 0;
-    for(i=1;i<=16;i=i+1) begin
+    for(i=0;i<16;i=i+1) begin
         ONES = ONES + DATA[i];
     end
 end
@@ -168,21 +236,19 @@ always @ (posedge CLK40)
 wire [11:0] TDC_VAL;
 assign TDC_VAL = TDC_PRE + ONES;
 
-
 reg [15:0] EVENT_CNT;
 always @ (posedge CLK40)
     if(RST_SYNC)
         EVENT_CNT <= 0;
     else if (FINISH)
         EVENT_CNT <= EVENT_CNT +1;
-    
-///
+
 wire wfull;
 wire cdc_fifo_write;
 assign cdc_fifo_write = !wfull && FINISH;
 
 wire [31:0] cdc_data;
-assign cdc_data = {IDENTYFIER, EVENT_CNT, TDC_VAL};
+assign cdc_data = {DATA_IDENTIFIER, EVENT_CNT, TDC_VAL};
 
 always@(posedge CLK40) begin
     if(RST_SYNC)
