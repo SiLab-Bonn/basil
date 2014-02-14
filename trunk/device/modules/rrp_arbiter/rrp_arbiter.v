f65956fc494addc28a7ed7cb364a560b7ba60da7
jejan <jejan@6913ced5-e916-43c7-8107-7318e977fe46>
2014-02-14 17:21:02 +0000

    ENH: changed select to one hot style and simplified design for higher implementation speed

--- trunk/device/modules/rrp_arbiter/rrp_arbiter.v
+++ trunk/device/modules/rrp_arbiter/rrp_arbiter.v
@@ -9,17 +9,18 @@
  *  $Date::                      $:
  */
  
+//TODO: check with more then 1 hold/priority at a time
+
 module rrp_arbiter 
 #(
-    parameter WIDTH = 4,
-    parameter PRIORITY = 0
+    parameter WIDTH = 4
 )
 (
     input RST,
     input CLK,
     
-    input [WIDTH-1:0] WRITE_REQ,
-    input [WIDTH-1:0] HOLD_REQ,
+    input [WIDTH-1:0] WRITE_REQ, // round robin
+    input [WIDTH-1:0] HOLD_REQ, // lower channels have higher priority, has to be high until read was granted
     input [WIDTH*32-1:0] DATA_IN,
     output [WIDTH-1:0] READ_GRANT,
 
@@ -29,84 +30,85 @@ module rrp_arbiter
     
 );
 
-`include "../includes/log2func.v"
-
-assign WRITE_OUT = |WRITE_REQ && !(|HOLD_REQ);
-
-localparam SEL_SIZE = log2(WIDTH);
+//`include "../includes/log2func.v"
+//localparam SEL_SIZE = log2(WIDTH);
 
 integer m;
-reg [SEL_SIZE-1:0] prev_select;
-reg [SEL_SIZE-1:0] select;
-
-wire [WIDTH-1:0] PRIORITY_WRITE_REQ;
-assign PRIORITY_WRITE_REQ = PRIORITY & WRITE_REQ;
+reg [WIDTH-1:0] prev_select; // one hot
+reg [WIDTH-1:0] select; // one hot
 
 reg normal_mode;
 reg hold;
 
+wire WRITE_REQ_OR, HOLD_REQ_OR;
+assign WRITE_REQ_OR = |WRITE_REQ;
+assign HOLD_REQ_OR = |HOLD_REQ;
+
+assign WRITE_OUT = |(WRITE_REQ & select);
+//assign WRITE_OUT = HOLD_REQ_OR ? WRITE_REQ[0] : WRITE_REQ_OR;
+
 always@(*) begin
-    normal_mode = 0;
-    select = 0;
-    if( |HOLD_REQ && !hold) begin       
+    select = prev_select;
+    if(HOLD_REQ_OR && !hold) begin       
         m = 0;
-        select = m;
-        while(!HOLD_REQ[select] && (m < WIDTH)) begin
+        select = 1; // always start from lowest channel
+        while(!(HOLD_REQ & select) && (m < WIDTH)) begin
             m = m + 1;
-            select = m;
-        end 
-    end
-    else if( |PRIORITY_WRITE_REQ && !hold ) begin
-        m = 0;
-        select = m;
-        while(!PRIORITY_WRITE_REQ[select] && (m < WIDTH)) begin
-            m = m+1;
-            select = m;
-        end 
-    end
-    else if( |WRITE_REQ ) begin
-        normal_mode = 1;
-        
-        if(hold == 0) begin
-                m = 0;
-                select = prev_select + 1;
-                while(WRITE_REQ[select] != 1 && (m < WIDTH)) begin
-                    m = m + 1;
-                    select = select + 1;
-                end
+            select = 1 << m;
         end
-        else
-             select = prev_select;
+    end
+    else if(WRITE_REQ_OR && !hold) begin
+        m = 0;
+        select = prev_select << 1; // start from last channel + 1
+        if(select == 0) // 0 is not valid
+            select = 1;
+        while(!(WRITE_REQ & select) && (m < WIDTH)) begin
+            m = m + 1;
+            select = select << 1;
+            if(select == 0) // 0 is not valid
+                select = 1;
+        end
     end
 end
 
 always@(posedge CLK) begin
     if(RST)
-        prev_select <= -1;
-    else if ( normal_mode & !hold)
+        prev_select <= (1 << (WIDTH - 1));
+    else if (WRITE_REQ_OR & !hold)
         prev_select <= select;
 end
 
 always@(posedge CLK) begin
     if(RST)
         hold <= 0;
-    else if( READY_OUT )
+    else if(READY_OUT)
         hold <= 0;
-    else if ( normal_mode || |PRIORITY_WRITE_REQ || |(HOLD_REQ & WRITE_REQ) )
+    else if (WRITE_OUT)
         hold <= 1;
 end
 
+//wire [31:0] DATA_A [WIDTH-1:0];
 
-wire [31:0] DATA_A [WIDTH-1:0];
+wire [WIDTH:0] DATA_A [31:0]; // this will simplify the loop below
 
-genvar i;
+// generation of DATA_A
+genvar i, j;
 generate
-  for (i = 0; i < WIDTH; i = i + 1) begin: gen
-    assign DATA_A[i] = DATA_IN[(i+1)*32-1:i*32];
-  end
+    for (i = 0; i < 32; i = i + 1) begin: gen
+        for (j = 0; j < WIDTH; j = j + 1) begin: gen2
+            assign DATA_A[i][j] = DATA_IN[j*32+i];
+        end
+    end
 endgenerate
 
-assign DATA_OUT = DATA_A[select];
-assign READ_GRANT = 1<<select & {WIDTH{READY_OUT}};
- 
+// selecting bits for DATA_OUT
+generate
+    for (i = 0; i < 32; i = i + 1) begin: gen3
+        assign DATA_OUT[i] = |(DATA_A[i] & select);
+    end
+endgenerate
+
+//assign DATA_OUT = DATA_A[select];
+assign READ_GRANT = select & {WIDTH{READY_OUT}};
+
 endmodule
