34b893ca9544a1532804c5da6d87779947fdcdbd
Tomasz Hemperek <themperek@users.noreply.github.com>
2014-05-07 20:54:18 +0000

    ENH:add memory longing to Dummy

--- host/pydaq/TL/Dummy.py
+++ host/pydaq/TL/Dummy.py
@@ -15,6 +15,8 @@ import array
 
 class Dummy (TransferLayer):
 
+    mem = dict()
+
     def __init__(self, conf):
         TransferLayer.__init__(self, conf)
 
@@ -26,6 +28,10 @@ class Dummy (TransferLayer):
         #import traceback
         #for line in traceback.format_stack():
         #    print line.strip()
+        address = addr
+        for d in data:
+            self.mem[address] = d
+            address += 1
 
         print "DummyTransferLayer.write addr:", addr, "data:", data
 
