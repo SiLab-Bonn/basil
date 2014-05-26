8c63bf1e21ca1ca2084f6f9439c93c264171b50d
jejan <jejan@6913ced5-e916-43c7-8107-7318e977fe46>
2014-05-26 17:30:25 +0000

    ENH: driver for TLU FPGA module

--- trunk/host/pydaq/HL/tlu.py
+++ trunk/host/pydaq/HL/tlu.py
@@ -10,9 +10,9 @@
 #
 
 from HL.HardwareLayer import HardwareLayer
+from struct import pack, unpack, unpack_from
+from array import array
 
-import struct
-import array
 
 trigger_modes = {
     0: 'external trigger',
@@ -21,6 +21,7 @@ trigger_modes = {
     3: 'TLU trigger data handshake'
 }
 
+
 class tlu(HardwareLayer):
     '''
     TLU controller interface
@@ -28,88 +29,98 @@ class tlu(HardwareLayer):
     def __init__(self, intf, conf):
         HardwareLayer.__init__(self, intf, conf)
 
-
-    '''
-    Resets the TLU controller module inside the FPGA, base adress zero
-    '''                                   
-    def reset(self):
-        self._intf.write(self._conf['base_addr'], [0])
-
-    '''
-    Initialise the TLU controller module
-    '''                                   
     def init(self):
         self.reset()
-        
-#    '''
-#    Sets the mode , at which adresse is this???
-#    '''                                   
-#    def set_mode(self , addr, data):
-#        self._intf.write(self._conf['base_addr'] + 16 + addr, data)
 
-    def get_tlu_trigger_number(self):
-        '''Reading most recent TLU trigger data/number.
-        '''
-        trigger_number_array = self._intf.read(self._conf['base_addr'] + 4, size=4)
-        return struct.unpack('I', trigger_number_array)[0]
-    
-    def get_trigger_number(self):
-        '''Reading internal trigger counter.
-        '''
-        trigger_number_array = self._intf.read(self._conf['base_addr'] + 8, size=4)
-        return struct.unpack('I', trigger_number_array)[0]
-    
-#    def set_trigger_number(self, value=0):
-    def set_trigger_number(self, value):
-        '''Writing ???? internal trigger counter.
-        '''
-        trigger_number = array.array('B', struct.pack('I', value))
-        self._intf.write(self._conf['base_addr'] + 8, data=trigger_number)
+    def reset(self):
+        self._intf.write(self._conf['base_addr'], (0,))
+        self.reset()
 
-    def configure_trigger_fsm(self, mode=0, trigger_data_msb_first=False, disable_veto=False, trigger_data_delay=0, trigger_clock_cycles=16, enable_reset=False, invert_lemo_trigger_input=False, trigger_low_timeout=0):
-        '''Setting up external trigger mode and TLU trigger FSM.
-    
-        Parameters
-        ----------
-        mode : string
-            TLU handshake mode. External trigger has to be enabled in command FSM. From 0 to 3.
-            0: External trigger (LEMO RX0 only, TLU port disabled (TLU port/RJ45)).
-            1: TLU no handshake (automatic detection of TLU connection (TLU port/RJ45)).
-            2: TLU simple handshake (automatic detection of TLU connection (TLU port/RJ45)).
-            3: TLU trigger data handshake (automatic detection of TLU connection (TLU port/RJ45)).
-        trigger_data_msb_first : bool
-            Setting endianness of TLU trigger data.
-        disable_veto : bool
-            Disabling TLU veto support.
-        trigger_data_delay : int
-            Addition wait cycles before latching TLU trigger data. From 0 to 15.
-        trigger_clock_cycles : int
-            Number of clock cycles sent to TLU to clock out TLU trigger data. The number of clock cycles is usually (bit length of TLU trigger data + 1). From 0 to 31.
-        enable_reset : bool
-            Enable resetting of internal trigger counter when TLU asserts reset signal.
-        invert_lemo_trigger_input : bool
-            Enable inverting of LEMO RX0 trigger input.
-        trigger_low_timeout : int
-            Enabling timeout for waiting for de-asserting TLU trigger signal. From 0 to 255.
+    def set_trigger_mode(self, value):
+        if value not in trigger_modes.iterkeys():
+            raise ValueError('Trigger mode does not exist')
+        ret = self._intf.read(self._conf['base_addr'] + 1, size=1)
+        reg = unpack_from('B', ret)[0]
+        reg = (value & 0x03) | (reg & 0xfc)
+        self._intf.write(self._conf['base_addr'] + 1, data=(reg,))
+
+    def set_trigger_msb_first(self, value):
+        ret = self._intf.read(self._conf['base_addr'] + 1, size=1)
+        reg = unpack_from('B', ret)[0]
+        if value:
+            reg |= 0x04
+        else:
+            reg &= ~0x04
+        self._intf.write(self._conf['base_addr'] + 1, data=(reg,))
+
+    def set_veto(self, value):
+        ret = self._intf.read(self._conf['base_addr'] + 1, size=1)
+        reg = unpack_from('B', ret)[0]
+        if value:
+            reg |= 0x08
+        else:
+            reg &= ~0x08
+        self._intf.write(self._conf['base_addr'] + 1, data=(reg,))
+
+    def set_trigger_data_delay(self, value):
+        if value < 0 or value > 15:
+            raise ValueError('Value exceeds limits')
+        ret = self._intf.read(self._conf['base_addr'] + 1, size=1)
+        reg = unpack_from('B', ret)[0]
+        reg = ((value & 0x0f) << 4) | (reg & 0x0f)
+        self._intf.write(self._conf['base_addr'] + 1, data=(reg,))
+
+    def set_trigger_clock_cycles(self, value):
+        if value < 0 or value > 31:
+            raise ValueError('Value exceeds limits')
+        ret = self._intf.read(self._conf['base_addr'] + 2, size=1)
+        reg = unpack_from('B', ret)[0]
+        reg = (value & 0x1f) | (reg & 0xe0)
+        self._intf.write(self._conf['base_addr'] + 2, data=(reg,))
+
+    def set_tlu_reset(self, value):
+        ret = self._intf.read(self._conf['base_addr'] + 2, size=1)
+        reg = unpack_from('B', ret)[0]
+        if value:
+            reg |= 0x20
+        else:
+            reg &= ~0x20
+        self._intf.write(self._conf['base_addr'] + 2, data=(reg,))
+
+    def set_invert_trigger(self, value):
+        ret = self._intf.read(self._conf['base_addr'] + 2, size=1)
+        reg = unpack_from('B', ret)[0]
+        if value:
+            reg |= 0x40
+        else:
+            reg &= ~0x40
+        self._intf.write(self._conf['base_addr'] + 2, data=(reg,))
+
+    def set_write_timestamp(self, value):
+        ret = self._intf.read(self._conf['base_addr'] + 2, size=1)
+        reg = unpack_from('B', ret)[0]
+        if value:
+            reg |= 0x80
+        else:
+            reg &= ~0x80
+        self._intf.write(self._conf['base_addr'] + 2, data=(reg,))
+
+    def set_trigger_low_timeout(self, value):
+        self._intf.write(self._conf['base_addr'] + 3, array.array('B', pack('B', value)))  # alternatively: unpack('B', pack('B', value))
+
+    def get_current_tlu_trigger_number(self):
+        '''Reading current trigger number.
         '''
-        reg_1 = (mode & 0x03)
-        if trigger_data_msb_first:
-            reg_1 |= 0x04
-        else:
-            reg_1 &= ~0x04
-        if disable_veto:
-            reg_1 |= 0x08
-        else:
-            reg_1 &= ~0x08
-        reg_1 = ((trigger_data_delay & 0x0f) << 4) | (reg_1 & 0x0f)
-        reg_2 = (trigger_clock_cycles & 0x1F)  # 0 = 32 clock cycles
-        if enable_reset:
-            reg_2 |= 0x20
-        else:
-            reg_2 &= ~0x20
-        if invert_lemo_trigger_input:
-            reg_2 |= 0x40
-        else:
-            reg_2 &= ~0x40
-        reg_3 = trigger_low_timeout
-        self._intf.write(self._conf['base_addr'] + 1, data=[reg_1, reg_2, reg_3])  # overwriting registers
+        ret = self._intf.read(self._conf['base_addr'] + 4, size=4)
+        return unpack_from('I', ret)[0]
+
+    def set_trigger_counter(self, value):
+        '''Setting trigger counter.
+        '''
+        self._intf.write(self._conf['base_addr'] + 8, array.array('B', pack('I', value)))
+
+    def get_trigger_counter(self):
+        '''Reading trigger counter.
+        '''
+        ret = self._intf.read(self._conf['base_addr'] + 8, size=4)
+        return unpack_from('I', ret)[0]
