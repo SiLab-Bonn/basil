67eeea54063b71c9450fd1b792e2250240643a50
jejan <jejan@6913ced5-e916-43c7-8107-7318e977fe46>
2014-06-04 14:00:33 +0000

    BUG: fix API change

--- trunk/host/RL/TrackRegister.py
+++ trunk/host/RL/TrackRegister.py
@@ -15,7 +15,8 @@ from utils import utils
 
 
 class TrackRegister(RegisterLayer):
-
+    '''Tracking register
+    '''
     def __init__(self, driver, conf):
         RegisterLayer.__init__(self, driver, conf)
         self._tracks = dict()
@@ -23,7 +24,7 @@ class TrackRegister(RegisterLayer):
         for track in self._conf['tracks']:
                 bv = BitVector(size=self._conf["seq_size"])
                 self._tracks[track['name']] = bv
-                
+
     def __getitem__(self, items):
         if items in self._tracks:
             return  self._tracks[items]
@@ -37,7 +38,7 @@ class TrackRegister(RegisterLayer):
         'Clear tracks in memory - all zero'
         for track in self._tracks:
             self._tracks[track].reset(0)
-        
+
     def write(self, size=-1):
         if size == -1:
             size = self._conf["seq_size"]
@@ -55,8 +56,7 @@ class TrackRegister(RegisterLayer):
                         bit = (i - 1) * self._conf["seq_width"] + self._conf["seq_width"] - 1 - track['position']
                 else:
                     raise NotImplementedError("To be implemented.")
-                                  
                 bv[bit] = self._tracks[track['name']][i]
-        
+
         ba = utils.bitvector_to_byte_array(bv)
-        self._drv.set_data(0, ba)  # TODO: this probably has to be done diffrent way
+        self._drv.set_data(ba)  # TODO: this probably has to be done different way
