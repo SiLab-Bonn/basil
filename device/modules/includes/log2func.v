fa6e3247fbf1d7da10a1e8e25fddc979492eddda
Tomasz Hemperek <themperek@users.noreply.github.com>
2014-06-06 20:19:34 +0000

    ENH: Log2 fucntion that works on icarus (by define) ENH: seq_gen no memory for 8bit data better DONE

--- device/modules/includes/log2func.v
+++ device/modules/includes/log2func.v
@@ -1,17 +1,24 @@
 
-function integer log2;
+
+`ifdef _IVERILOG_
+	`define CLOG2 $clog2
+`else
+	function integer clog2;
     input integer value;
     reg [31:0] shifted;
     integer res;
         begin
         if (value < 2)
-          log2 = value;
+          clog2 = value;
         else
         begin
           shifted = value-1;
           for (res=0; shifted>0; res=res+1)
             shifted = shifted>>1;
-          log2 = res;
+          clog2 = res;
         end
     end
-endfunction
+	endfunction
+
+	`define CLOG2 clog2
+`endif 
