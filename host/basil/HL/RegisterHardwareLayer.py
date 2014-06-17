80cc9ae34676058fd855878bbd53cfc59dae8d81
Jens Janssen <janssen@physik.uni-bonn.de>
2014-06-17 21:53:09 +0000

    SVN: adding RegisterHardwareLayer.py for easy access of registers, see unittest for an example

---
+++ host/basil/HL/RegisterHardwareLayer.py
@@ -0,0 +1,58 @@
+#
+# ------------------------------------------------------------
+# Copyright (c) All rights reserved
+# SiLab, Institute of Physics, University of Bonn
+# ------------------------------------------------------------
+#
+# SVN revision information:
+#  $Rev:: 304                   $:
+#  $Author:: jejan              $:
+#  $Date:: 2014-06-10 17:49:43 #$:
+#
+
+
+from basil.HL.HardwareLayer import HardwareLayer
+
+
+class RegisterHardwareLayer(HardwareLayer):
+    '''Register Hardware Layer.
+
+    Implementation of advanced register operations.
+    '''
+    _registers = {}
+
+    def __init__(self, intf, conf):
+        super(RegisterHardwareLayer, self).__init__(intf, conf)
+        for item in self._registers.iterkeys():
+            self.add_property(item)
+
+    def init(self):
+        pass
+
+    def add_property(self, attribute):
+        # create local setter and getter with a particular attribute name
+        getter = lambda self: self.get(attribute)
+        setter = lambda self, value: self.set(attribute, value)
+
+        # construct property attribute and add it to the class
+        setattr(self.__class__, attribute, property(fget=getter, fset=setter, doc=attribute + ' register'))
+
+    def set_default(self):
+        for reg in self._registers.iterkeys():
+            self.set(reg, self._registers[reg]['default'])
+
+    def get(self, reg):
+        descr = self._registers[reg]['descr']
+        descr.setdefault('offset', 0)
+        curr_val = self._registers[reg].setdefault('current', None)
+        ret_val = self._get_value(**descr)
+        print reg, curr_val, ret_val
+        if curr_val and curr_val != ret_val:
+            raise ValueError('Read value is not expected')
+        return ret_val
+
+    def set(self, reg, value):
+        descr = self._registers[reg]['descr']
+        descr.setdefault('offset', 0)
+        self._registers[reg].update({'current': value if isinstance(value, (int, long)) else int(value, base=2)})
+        self._set_value(value, **descr)
---
+++ host/basil/tests/test_RegisterHardwareLayer.py
@@ -0,0 +1,74 @@
+#
+# ------------------------------------------------------------
+# Copyright (c) All rights reserved
+# SiLab, Institute of Physics, University of Bonn
+# ------------------------------------------------------------
+#
+# SVN revision information:
+#  $Rev:: 261                   $:
+#  $Author:: jejan              $:
+#  $Date:: 2014-06-06 15:16:45 #$:
+#
+
+import unittest
+
+from basil.HL.RegisterHardwareLayer import RegisterHardwareLayer
+from basil.TL.Dummy import Dummy
+
+
+class MyHardwareLayer(RegisterHardwareLayer):
+    '''Register Hardware Layer.
+
+    Implementation of advanced register operations.
+    '''
+    _registers = {'REG1': {'default': 12, 'current': None, 'descr': {'addr': 0, 'size': 15, 'offset': 0}},
+                  'REG2': {'default': 1, 'current': None, 'descr': {'addr': 1, 'size': 1, 'offset': 7}},
+                  'REG3': {'default': 2 ** 16 - 1, 'current': None, 'descr': {'addr': 2, 'size': 16, 'offset': 0}},
+    }
+
+
+class TestRegisterHardwareLayer(unittest.TestCase):
+    def setUp(self):
+        dummy = Dummy(conf=None)
+        self.hl = MyHardwareLayer(dummy, {'base_addr': 0})
+
+    def test_set_default(self):
+        self.hl.set_default()
+        self.assertDictEqual({0: 12, 1: 128, 2: 255, 3: 255}, self.hl._intf.mem)
+
+    def test_write_read_reg(self):
+        for reg in self.hl._registers.iterkeys():
+            val = self.hl._registers[reg]['default']
+            self.hl.set(reg, val)
+            ret_val = self.hl.get(reg)
+            self.assertEqual(ret_val, val)
+        self.assertDictEqual({0: 12, 1: 128, 2: 255, 3: 255}, self.hl._intf.mem)
+
+    def test_set_attribute_by_value(self):
+        mem = self.hl._intf.mem
+        self.hl.REG2 = 0
+        mem[1] += 128
+        self.assertDictEqual(mem, self.hl._intf.mem)
+
+    def test_set_attribute_by_string(self):
+        mem = self.hl._intf.mem
+        self.hl.REG3 = '1010101010101010'
+        mem[2] = 170
+        mem[3] = 170
+        self.assertDictEqual(mem, self.hl._intf.mem)
+        print self.hl._intf.mem
+
+    def test_get_attribute_by_string(self):
+        self.hl.REG3 = '1010101010101010'  # 43690
+        self.assertEqual(43690, self.hl.REG3)
+
+    def test_set_attribute_too_long_string(self):
+        val = '11010101010101010'  # 17 bit
+        self.assertRaises(ValueError, self.hl.set, 'REG3', value=val)
+
+    def test_set_attribute_too_big_val(self):
+        val = 2 ** 16  # max 2 ** 16 - 1
+        self.assertRaises(ValueError, self.hl.set, 'REG3', value=val)
+
+if __name__ == '__main__':
+    unittest.main()
