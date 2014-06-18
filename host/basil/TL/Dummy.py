89275908539e4eea044938ba81f3d6d9ac2b2754
Tomasz Hemperek <themperek@users.noreply.github.com>
2014-06-18 05:47:28 +0000

    BUG: Fix Dummy TL

--- host/basil/TL/Dummy.py
+++ host/basil/TL/Dummy.py
@@ -32,10 +32,10 @@ class Dummy(TransferLayer):
         #import traceback
         #for line in traceback.format_stack():
         #    print line.strip()
-        print "DummyTransferLayer.write addr:", addr, "data:", data
+        print "DummyTransferLayer.write addr:", hex(addr), "data:", data
         for curr_addr, d in enumerate(data, start=addr):
-            self.mem[curr_addr] = unpack('B', d)[0]
+            self.mem[curr_addr] = array.array('B', [d] )[0] #unpack('B', d)[0]
 
     def read(self, addr, size):
-        print "DummyTransferLayer.read addr:", addr, "size:", size
+        print "DummyTransferLayer.read addr:", hex(addr), "size:", size
         return array.array('B', [self.mem[curr_addr] if curr_addr in self.mem else 0 for curr_addr in range(addr, addr + size)])
