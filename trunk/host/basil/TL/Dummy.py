e59928f89a12c0ddcd45a2873220ce3bb5c15767
jejan <jejan@6913ced5-e916-43c7-8107-7318e977fe46>
2014-06-18 14:12:59 +0000

    MAINT: documentation

--- trunk/host/basil/TL/Dummy.py
+++ trunk/host/basil/TL/Dummy.py
@@ -13,8 +13,6 @@
 from basil.TL.TransferLayer import TransferLayer
 import array
 
-from struct import unpack
-
 
 class Dummy(TransferLayer):
     '''Dummy device
@@ -29,13 +27,36 @@ class Dummy(TransferLayer):
         print "Conf:", str(self._conf)
 
     def write(self, addr, data):
-        #import traceback
-        #for line in traceback.format_stack():
-        #    print line.strip()
+        '''Write to dummy memory
+
+        Parameters
+        ----------
+        addr : int
+            The register address.
+        data : list, tuple
+            Data (byte array) to be written.
+
+        Returns
+        -------
+        nothing
+        '''
         print "DummyTransferLayer.write addr:", hex(addr), "data:", data
         for curr_addr, d in enumerate(data, start=addr):
-            self.mem[curr_addr] = array.array('B', [d] )[0] #unpack('B', d)[0]
+            self.mem[curr_addr] = array.array('B', [d])[0]
 
     def read(self, addr, size):
+        '''
+        Parameters
+        ----------
+        addr : int
+            The register address.
+        size : int
+            Length of data to be read (number of bytes).
+
+        Returns
+        -------
+        array : array
+            Data (byte array) read from memory. Returns 0 for each byte if it hasn't been written to.
+        '''
         print "DummyTransferLayer.read addr:", hex(addr), "size:", size
         return array.array('B', [self.mem[curr_addr] if curr_addr in self.mem else 0 for curr_addr in range(addr, addr + size)])
