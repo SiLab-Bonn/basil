0333cb7fdd29e3eea3b1a63edc9375fae418598c
Jens Janssen <janssen@physik.uni-bonn.de>
2014-06-18 14:12:23 +0000

    ENH: added read_only and write_only properties API: hide get and set functions ENH: more tests

--- host/basil/HL/RegisterHardwareLayer.py
+++ host/basil/HL/RegisterHardwareLayer.py
@@ -13,6 +13,9 @@
 
 from basil.HL.HardwareLayer import HardwareLayer
 
+read_only = ['read-only', 'readonly', 'ro']
+write_only = ['write-only', 'writeonly', 'wo']
+
 
 class RegisterHardwareLayer(HardwareLayer):
     '''Register Hardware Layer.
@@ -31,18 +34,21 @@ class RegisterHardwareLayer(HardwareLayer):
 
     def add_property(self, attribute):
         # create local setter and getter with a particular attribute name
-        getter = lambda self: self.get(attribute)
-        setter = lambda self, value: self.set(attribute, value)
+        getter = lambda self: self._get(attribute)
+        setter = lambda self, value: self._set(attribute, value)
 
         # construct property attribute and add it to the class
         setattr(self.__class__, attribute, property(fget=getter, fset=setter, doc=attribute + ' register'))
 
     def set_default(self):
-        for reg in self._registers.iterkeys():
-            self.set(reg, self._registers[reg]['default'])
+        for reg, value in self._registers.iteritems():
+            if not 'properties' in value['descr'] or not [i for i in read_only if i in self._registers[reg]['descr']['properties']]:
+                self._set(reg, value['default'])
 
-    def get(self, reg):
+    def _get(self, reg):
         descr = self._registers[reg]['descr']
+        if 'properties' in descr and [i for i in write_only if i in descr['properties']]:
+            raise IOError('Value is write-only')
         descr.setdefault('offset', 0)
         curr_val = self._registers[reg].setdefault('current', None)
         ret_val = self._get_value(**descr)
@@ -50,14 +56,16 @@ class RegisterHardwareLayer(HardwareLayer):
             raise ValueError('Read value is not expected')
         return ret_val
 
-    def set(self, reg, value):
+    def _set(self, reg, value):
         descr = self._registers[reg]['descr']
+        if 'properties' in descr and [i for i in read_only if i in descr['properties']]:
+            raise IOError('Value is read-only')
         descr.setdefault('offset', 0)
         self._registers[reg].update({'current': value if isinstance(value, (int, long)) else int(value, base=2)})
         self._set_value(value, **descr)
 
     def __getitem__(self, name):
-        return self.get(name)
+        return self._get(name)
 
     def __setitem__(self, name, value):
-        return self.set(name, value)
+        return self._set(name, value)
