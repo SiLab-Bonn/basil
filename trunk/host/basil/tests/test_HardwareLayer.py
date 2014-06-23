443e772ea4c9818888a5761a26309a8953c29946
jejan <jejan@6913ced5-e916-43c7-8107-7318e977fe46>
2014-06-23 15:32:39 +0000

    ENH: use Dut and yaml

--- trunk/host/basil/tests/test_HardwareLayer.py
+++ trunk/host/basil/tests/test_HardwareLayer.py
@@ -12,43 +12,41 @@
 
 import unittest
 
-from basil.HL.HardwareLayer import HardwareLayer
-from basil.TL.Dummy import Dummy
+from basil.dut import Dut
 
 
 class TestHardwareLayer(unittest.TestCase):
     def setUp(self):
-        dummy = Dummy(conf=None)
-        self.hl = HardwareLayer(dummy, {'base_addr': 0})
+        self.dut = Dut('test_RegisterHardwareLayer.yaml')
 
     def test_write_read_reg(self):
         for val in range(256):
-            self.hl._set_value(val, 0, size=8, offset=0)
-            ret_val = self.hl._get_value(0, size=8, offset=0)
+            self.dut['test_register']._set_value(val, 0, size=8, offset=0)
+            ret_val = self.dut['test_register']._get_value(0, size=8, offset=0)
             self.assertEqual(ret_val, val)
 
     def test_write_read_reg_with_bit_str(self):
         val = '00110110'  # 54
-        self.hl._set_value(val, 0, size=8, offset=0)
-        ret_val = self.hl._get_value(0, size=8, offset=0)
+        self.dut['test_register']._set_value(val, 0, size=8, offset=0)
+        ret_val = self.dut['test_register']._get_value(0, size=8, offset=0)
         self.assertEqual(ret_val, int(val, base=2))
 
     def test_write_read_reg_with_offset(self):
         for offset in range(32):
             val = 131
-            self.hl._set_value(val, 0, size=8, offset=offset)
-            ret_val = self.hl._get_value(0, size=8, offset=offset)
+            self.dut['test_register']._set_value(val, 0, size=8, offset=offset)
+            ret_val = self.dut['test_register']._get_value(0, size=8, offset=offset)
             self.assertEqual(ret_val, val)
 
     def test_write_read_reg_with_size(self):
         for size in range(8, 33):
             val = 131
-            self.hl._set_value(val, 0, size=size, offset=7)
-            ret_val = self.hl._get_value(0, size=size, offset=7)
+            self.dut['test_register']._set_value(val, 0, size=size, offset=7)
+            ret_val = self.dut['test_register']._get_value(0, size=size, offset=7)
             self.assertEqual(ret_val, val)
 
     def test_wrong_size(self):
-        self.assertRaises(ValueError, self.hl._set_value, 131, addr=0, size=7, offset=7)
+        self.assertRaises(ValueError, self.dut['test_register']._set_value, 131, addr=0, size=7, offset=7)
 
 if __name__ == '__main__':
     unittest.main()
