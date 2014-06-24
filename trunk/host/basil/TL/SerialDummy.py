54d250efa9d66e1600ce3e5243df26efd5a3fb02
thirono <thirono@6913ced5-e916-43c7-8107-7318e977fe46>
2014-06-24 07:07:28 +0000

    add hw_drivers for Agilent 33250a and SHQ122m

---
+++ trunk/host/basil/TL/SerialDummy.py
@@ -0,0 +1,28 @@
+#
+# ------------------------------------------------------------
+# Copyright (c) All rights reserved
+# SiLab, Institute of Physics, University of Bonn
+# ------------------------------------------------------------
+#
+# SVN revision information:
+#  $Rev:: 369                   $:
+#  $Author:: jejan              $:
+#  $Date:: 2014-06-18 16:12:59 #$:
+#
+
+from basil.TL.TransferLayer import TransferLayer
+
+class SerialDummy(TransferLayer):
+    '''Dummy class for serial device
+    ''' 
+    def __init__(self, conf):
+        pass
+
+    def init(self):
+        return 
+
+    def write(self, addr, data):
+        pass
+
+    def read(self, addr, size):
+        pass
