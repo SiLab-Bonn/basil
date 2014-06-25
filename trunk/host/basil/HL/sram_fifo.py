ff534a0c224ee7ff95d36b2fa74bac7d7ac5f97d
jejan <jejan@6913ced5-e916-43c7-8107-7318e977fe46>
2014-06-25 20:43:16 +0000

    ENH: use RegisterHardwareLayer

--- trunk/host/basil/HL/sram_fifo.py
+++ trunk/host/basil/HL/sram_fifo.py
@@ -7,16 +7,24 @@
 #  $Date::                      $:
 #
 
-from basil.HL.HardwareLayer import HardwareLayer
+from basil.HL.RegisterHardwareLayer import RegisterHardwareLayer
 from struct import pack, unpack_from
 from array import array
 from time import sleep
 import numpy as np
 
 
-class sram_fifo(HardwareLayer):
+class sram_fifo(RegisterHardwareLayer):
     '''SRAM FIFO controller interface for sram_fifo FPGA module.
     '''
+
+    _registers = {'RESET': {'descr': {'addr': 0, 'size': 8, 'properties': ['writeonly']}},
+                  'ALMOST_FULL_THRESHOLD': {'descr': {'addr': 1, 'size': 8, 'properties': ['wo']}},
+                  'ALMOST_EMPTY_THRESHOLD': {'descr': {'addr': 2, 'size': 8, 'properties': ['wo']}},
+                  'FIFO_SIZE': {'descr': {'addr': 1, 'size': 21, 'properties': ['ro']}},
+                  'READ_ERROR_COUNTER': {'descr': {'addr': 4, 'size': 8, 'properties': ['ro']}}
+    }
+
     def __init__(self, intf, conf):
         super(sram_fifo, self).__init__(intf, conf)
 
@@ -46,6 +54,20 @@ class sram_fifo(HardwareLayer):
         ret.append(0)  # increase to 4 bytes to do the conversion
         return unpack_from('I', ret)[0]
 
+    @property
+    def FIFO_INT_SIZE(self):
+        '''
+        Get FIFO size in units of integers (32 bit).
+
+        Returns
+        -------
+        fifo_size : int
+            FIFO size in units of integers (32 bit).
+        '''
+        fifo_size = self.FIFO_SIZE
+        # sometimes reading of FIFO size happens during writing to SRAM, but we want to have a multiplicity of 32 bits
+        return (fifo_size - (fifo_size % 2)) / 2
+
     def get_fifo_int_size(self):
         '''
         Get FIFO size in units of integers (32 bit).
@@ -55,9 +77,7 @@ class sram_fifo(HardwareLayer):
         fifo_size : int
             FIFO size in units of integers (32 bit).
         '''
-        fifo_size = self.get_fifo_size()
-        # sometimes reading of FIFO size happens during writing to SRAM, but we want to have a multiplicity of 32 bits
-        return (fifo_size - (fifo_size % 2)) / 2
+        return self.FIFO_INT_SIZE
 
     def get_read_error_counter(self):
         '''
