5b9526c370b18116141a63ee67a68a4234e88e4d
Jens Janssen <janssen@physik.uni-bonn.de>
2014-07-24 13:04:15 +0000

    ENH: added CE

--- device/modules/utils/3_stage_synchronizer.v
+++ device/modules/utils/3_stage_synchronizer.v
@@ -38,3 +38,28 @@ endmodule
 //    out_d_ff_2 <= out_d_ff_1;
 //    OUT <= out_d_ff_2;
 //end
+
+
+module three_stage_synchronizer_ce #(
+    parameter WIDTH = 1
+) (
+    input wire                  CLK,
+    input wire                  CE,
+    input wire  [WIDTH-1:0]     IN,
+    output reg  [WIDTH-1:0]     OUT
+);
+
+reg [WIDTH-1:0] out_d_ff_1, out_d_ff_2;
+
+
+always @(posedge CLK) // first stage
+begin
+    if (CE)
+    begin
+        out_d_ff_1 <= IN;
+        out_d_ff_2 <= out_d_ff_1;
+        OUT <= out_d_ff_2;
+    end
+end
+
+endmodule
