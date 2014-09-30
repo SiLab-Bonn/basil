7212a34e4b2fc76ab991808c1c258e284d560d23
themperek <themperek@6913ced5-e916-43c7-8107-7318e977fe46>
2014-09-30 13:17:52 +0000

    BUG: Clear pattern bit for GPAC ADC configuration

--- trunk/host/basil/UL/FadcConf.py
+++ trunk/host/basil/UL/FadcConf.py
@@ -28,6 +28,9 @@ class FadcConf(UserLayer):
         self._drv.set_data([0x02, 0x07])  # SET 16 bit mode
         self._drv.start()
 
+        self._drv.set_data([0x03, 0x00]) # PATTERN OFF
+        self._drv.start()
+
         #print 'chip[\'FADC_CONF\'].IsDone() = ', dut['FADC_CONF'].IsDone()
 
         #dut['FADC_CONF'].SetData([0x82, 0x00]) # SET 16 bit mode
