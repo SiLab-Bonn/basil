899de3912d1e4946b90ce446bb1483159bf7c3cb
themperek <themperek@6913ced5-e916-43c7-8107-7318e977fe46>
2014-12-15 21:45:22 +0000

    ENH: One file for one module

--- trunk/device/modules/utils/three_stage_synchronizer_ce.v
+++ trunk/device/modules/utils/three_stage_synchronizer_ce.v
@@ -2,10 +2,12 @@
 
 // synchronizing asynchronous signals/flags, prevents metastable events
 
-module three_stage_synchronizer #(
+
+module three_stage_synchronizer_ce #(
     parameter WIDTH = 1
 ) (
     input wire                  CLK,
+    input wire                  CE,
     input wire  [WIDTH-1:0]     IN,
     output reg  [WIDTH-1:0]     OUT
 );
@@ -15,11 +17,12 @@ reg [WIDTH-1:0] out_d_ff_1, out_d_ff_2;
 
 always @(posedge CLK) // first stage
 begin
-    out_d_ff_1 <= IN;
-    out_d_ff_2 <= out_d_ff_1;
-    OUT <= out_d_ff_2;
+    if (CE)
+    begin
+        out_d_ff_1 <= IN;
+        out_d_ff_2 <= out_d_ff_1;
+        OUT <= out_d_ff_2;
+    end
 end
 
 endmodule
-
-
