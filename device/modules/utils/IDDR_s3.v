311d48521ab6ff4a05b85251a234544890fe31d7
Tomasz Hemperek <themperek@users.noreply.github.com>
2014-12-15 20:31:32 +0000

    ENH: Add IDDR_s3

---
+++ device/modules/utils/IDDR_s3.v
@@ -0,0 +1,22 @@
+/**
+ * ------------------------------------------------------------
+ * Copyright (c) SILAB , Physics Institute of Bonn University 
+ * ------------------------------------------------------------
+ *
+ */
+
+module IDDR ( output wire Q1, Q2, 
+                       input wire C, CE, D, R, S );
+
+IFDDRRSE IFDDRRSE_inst (
+    .Q0(Q1), 
+    .Q1(Q2), 
+    .C0(C), 
+    .C1(~C), 
+    .CE(CE), 
+    .D(D), 
+    .R(R), 
+    .S(S)
+);
+
+endmodule
