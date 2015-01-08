bb785f7b63d7d56cded1608987165961d9d952b8
jejan <jejan@6913ced5-e916-43c7-8107-7318e977fe46>
2015-01-08 12:12:39 +0000

    SVM: add for simulation

---
+++ trunk/device/modules/utils/IDDR_sim.v
@@ -0,0 +1,33 @@
+
+module IDDR ( output reg Q1, Q2, 
+              input wire C, CE, D, R, S );
+
+
+always@ (posedge C) begin
+    if (R==1'b1)
+        Q1 <= 1'b0;
+    else if (S==1'b1)
+        Q1 <= 1'b1;
+    else if (CE)
+        if (D==1'b1)
+            Q1 <= 1'b1;
+        else if (D==1'b0)
+            Q1 <= 1'b0;
+end
+
+
+always@ (negedge C)
+begin
+    if (R==1'b1)
+        Q2 <= 1'b0;
+    else if (S==1'b1)
+        Q2 <= 1'b1;
+    else if (CE)
+        if (D==1'b1)
+            Q2 <= 1'b1;
+        else if (D==1'b0)
+            Q2 <= 1'b0;
+end                      
+
+
+endmodule
