567e5289e4d5f8fe9f32836dc68161f5efe3d02b
Jens Janssen <janssen@physik.uni-bonn.de>
2015-01-08 12:11:37 +0000

    ENH: do not constantly advance clock for reproducibility reasons MAINT: cleanup

--- host/basil/utils/sim/Test.py
+++ host/basil/utils/sim/Test.py
@@ -4,7 +4,7 @@
 # SiLab, Institute of Physics, University of Bonn
 # ------------------------------------------------------------
 #
-#Initial version by Chris Higgs <chris.higgs@potentialventures.com>
+# Initial version by Chris Higgs <chris.higgs@potentialventures.com>
 #
 
 import os
@@ -19,10 +19,12 @@ from importlib import import_module
 
 from Protocol import WriteRequest, ReadRequest, ReadResponse, PickleInterface
 
+
 def get_bus():
     bus_name_path = os.getenv("SIMULATION_BUS", "basil.utils.sim.BasilBusDriver")
     bus_name = bus_name_path.split('.')[-1]
     return getattr(__import__(bus_name_path, fromlist=[bus_name]), bus_name)
+
 
 @cocotb.test(skip=False)
 def socket_test(dut, debug=True):
@@ -30,49 +32,47 @@ def socket_test(dut, debug=True):
 
     host = os.getenv("SIMULATION_HOST", 'localhost')
     port = os.getenv("SIMULATION_PORT", '12345')
-    
+
     if debug:
         dut.log.setLevel(logging.DEBUG)
 
-    #get bus
+    # get bus
     bus = get_bus()(dut)
 
     dut.log.info("Using bus driver : %s" % (type(bus).__name__))
 
-    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((host, int(port)))
     s.listen(1)
-    
+
     yield bus.init()
-    
+
     while True:
         dut.log.info("Waiting for incoming connection on %s:%d" % (host, int(port)))
         client, sockname = s.accept()
         dut.log.info("New connection from %s:%d" % (sockname[0], sockname[1]))
         iface = PickleInterface(client)
-        
-        
-        
+
         while True:
-            
-            yield RisingEdge(bus.clock)
-            
+            # uncomment for constantly advancing clock
+#             yield RisingEdge(bus.clock)
+
             try:
                 req = iface.try_recv()
             except EOFError:
                 dut.log.info("Remote client closed the connection")
                 client.close()
                 break
-            if req is None: continue
+            if req is None:
+                continue
 
             dut.log.debug("Received: %s" % str(req))
-                
-            #add few clocks
-            for i in range(10):
+
+            # add few clocks
+            for _ in range(10):
                 yield RisingEdge(bus.clock)
-                    
+
             if isinstance(req, WriteRequest):
                 yield bus.write(req.address, req.data)
             elif isinstance(req, ReadRequest):
@@ -82,34 +82,34 @@ def socket_test(dut, debug=True):
                 iface.send(resp)
             else:
                 raise NotImplementedError("Unsupported request type: %s" % str(type(req)))
-            
-            #add few clocks
-            for i in range(10):
+
+            # add few clocks
+            for _ in range(10):
                 yield RisingEdge(bus.clock)
-        
+
         if(os.getenv("SIMULATION_END_ON_DISCONNECT")):
             break
+
 
 @cocotb.test(skip=True)
 def bringup_test(dut):
     """Initial test to see if simulation works"""
 
     bus = get_bus()(dut)
-    
+
     yield bus.init()
 
-    for i in range(10):
+    for _ in range(10):
         yield RisingEdge(bus.clock)
-        
-    yield bus.write(0, [0xff, 0xf2, 0xf3,0xa4])
 
-    for i in range(10):
+    yield bus.write(0, [0xff, 0xf2, 0xf3, 0xa4])
+
+    for _ in range(10):
         yield RisingEdge(bus.clock)
-    
-    ret = yield bus.read(0,4)
-    
+
+    ret = yield bus.read(0, 4)
+
     print 'bus.read', ret
-    
-    for i in range(10):
+
+    for _ in range(10):
         yield RisingEdge(bus.clock)
- 
