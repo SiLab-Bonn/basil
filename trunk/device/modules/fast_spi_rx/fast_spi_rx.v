165cecc92978fa9dcce7661499472bcc77db1862
jejan <jejan@6913ced5-e916-43c7-8107-7318e977fe46>
2015-01-08 13:06:46 +0000

    MAINT: cleanup

--- trunk/device/modules/fast_spi_rx/fast_spi_rx.v
+++ trunk/device/modules/fast_spi_rx/fast_spi_rx.v
@@ -29,46 +29,46 @@ module fast_spi_rx
     
 ); 
 
-    wire IP_RD, IP_WR;
-    wire [ABUSWIDTH-1:0] IP_ADD;
-    wire [7:0] IP_DATA_IN;
-    wire [7:0] IP_DATA_OUT;
-    
-    bus_to_ip #( .BASEADDR(BASEADDR), .HIGHADDR(HIGHADDR), .ABUSWIDTH(ABUSWIDTH) ) i_bus_to_ip
-    (
-        .BUS_RD(BUS_RD),
-        .BUS_WR(BUS_WR),
-        .BUS_ADD(BUS_ADD),
-        .BUS_DATA(BUS_DATA),
-    
-        .IP_RD(IP_RD),
-        .IP_WR(IP_WR),
-        .IP_ADD(IP_ADD),
-        .IP_DATA_IN(IP_DATA_IN),
-        .IP_DATA_OUT(IP_DATA_OUT)
-    );
-    
-    fast_spi_rx_core 
-    #(
-        .ABUSWIDTH(ABUSWIDTH),
-        .IDENTYFIER(IDENTYFIER)
-    ) i_fast_spi_rx_core 
-    (
-        .BUS_CLK(BUS_CLK),                     
-        .BUS_RST(BUS_RST),                  
-        .BUS_ADD(IP_ADD),                    
-        .BUS_DATA_IN(IP_DATA_IN),                    
-        .BUS_RD(IP_RD),                    
-        .BUS_WR(IP_WR),                    
-        .BUS_DATA_OUT(IP_DATA_OUT),
-          
-        .SCLK(SCLK),
-        .SDI(SDI),
-        .SEN(SEN),
-    
-        .FIFO_READ(FIFO_READ),
-        .FIFO_EMPTY(FIFO_EMPTY),
-        .FIFO_DATA(FIFO_DATA)
-    );
+wire IP_RD, IP_WR;
+wire [ABUSWIDTH-1:0] IP_ADD;
+wire [7:0] IP_DATA_IN;
+wire [7:0] IP_DATA_OUT;
+
+bus_to_ip #( .BASEADDR(BASEADDR), .HIGHADDR(HIGHADDR), .ABUSWIDTH(ABUSWIDTH) ) i_bus_to_ip
+(
+    .BUS_RD(BUS_RD),
+    .BUS_WR(BUS_WR),
+    .BUS_ADD(BUS_ADD),
+    .BUS_DATA(BUS_DATA),
+
+    .IP_RD(IP_RD),
+    .IP_WR(IP_WR),
+    .IP_ADD(IP_ADD),
+    .IP_DATA_IN(IP_DATA_IN),
+    .IP_DATA_OUT(IP_DATA_OUT)
+);
+
+fast_spi_rx_core 
+#(
+    .ABUSWIDTH(ABUSWIDTH),
+    .IDENTYFIER(IDENTYFIER)
+) i_fast_spi_rx_core 
+(
+    .BUS_CLK(BUS_CLK),                     
+    .BUS_RST(BUS_RST),                  
+    .BUS_ADD(IP_ADD),                    
+    .BUS_DATA_IN(IP_DATA_IN),                    
+    .BUS_RD(IP_RD),                    
+    .BUS_WR(IP_WR),                    
+    .BUS_DATA_OUT(IP_DATA_OUT),
+      
+    .SCLK(SCLK),
+    .SDI(SDI),
+    .SEN(SEN),
+
+    .FIFO_READ(FIFO_READ),
+    .FIFO_EMPTY(FIFO_EMPTY),
+    .FIFO_DATA(FIFO_DATA)
+);
 
 endmodule
