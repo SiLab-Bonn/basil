cc2d2289ddd5d93b72835add7bc422adf6c6e407
Jens Janssen <janssen@physik.uni-bonn.de>
2015-01-12 13:02:32 +0000

    MAINT: cleanup

--- device/modules/cmd_seq/cmd_seq_core.v
+++ device/modules/cmd_seq/cmd_seq_core.v
@@ -1,12 +1,8 @@
 /**
  * ------------------------------------------------------------
- * Copyright (c) SILAB , Physics Institute of Bonn University
+ * Copyright (c) All rights reserved 
+ * SiLab, Institute of Physics, University of Bonn
  * ------------------------------------------------------------
- *
- * SVN revision information:
- *  $Rev::                       $:
- *  $Author::                    $:
- *  $Date::                      $:
  */
 
 `timescale 1 ps / 1ps
