795aefa36256873044df270dca64261800302c1e
themperek <themperek@6913ced5-e916-43c7-8107-7318e977fe46>
2015-02-07 09:35:21 +0000

    BUG: Fix makefile generation for simulation

--- trunk/host/basil/utils/sim/utils.py
+++ trunk/host/basil/utils/sim/utils.py
@@ -17,12 +17,12 @@ def cocotb_makefile(sim_files, top_level='tb', test_module='basil.utils.sim.Test
         mkfile += "SIMULATION_END_ON_DISCONNECT?=1\n"
 
     mkfile += "\n"
-
+    
     mkfile += "VERILOG_SOURCES = %s\n\n" % (" ".join(str(e) for e in sim_files))
 
     mkfile += "TOPLEVEL = %s\nMODULE   = %s\n\n" % (top_level, test_module)
 
-    mkfile += "EXTRA_ARGS = -D_IVERILOG_ %s \n\n" % (" ".join('-I' + str(e) for e in include_dirs))
+    mkfile += "COMPILE_ARGS = -D_IVERILOG_ %s \n\n" % (" ".join('-I' + str(e) for e in include_dirs))
 
     mkfile += "VERILOG_INCLUDE_DIRS=./ %s\n"% (" ".join('+incdir+../' + str(e) for e in include_dirs)) #this is for modelsim better full path?
     
@@ -32,12 +32,17 @@ export SIMULATION_PORT
 export SIMULATION_BUS
 export SIMULATION_END_ON_DISCONNECT
 
+PYTHONLIBS=$(shell python -c 'from distutils import sysconfig; print(sysconfig.get_config_var("LIBDIR"))')
+export LD_LIBRARY_PATH=/lib/x86_64-linux-gnu:$(PYTHONLIBS)
+export PYTHONPATH=$(shell python -c "from distutils import sysconfig; print(sysconfig.get_python_lib())"):$(COCOTB)
+export PYTHONHOME=$(shell python -c "from distutils.sysconfig import get_config_var; print(get_config_var('prefix'))")
 
 TOPLEVEL_LANG?=verilog
 export TOPLEVEL_LANG
 
 include $(COCOTB)/makefiles/Makefile.inc
 include $(COCOTB)/makefiles/Makefile.sim
+
     """
 
     return mkfile
