0e626394bf7f872ed42055cb72a5191aa04e7341
Jens Janssen <janssen@physik.uni-bonn.de>
2015-03-03 15:29:31 +0100

    ENH: StdRegister checks length of GPIO and sets lenght for SPI during init

--- host/basil/HL/gpio.py
+++ host/basil/HL/gpio.py
@@ -18,11 +18,11 @@ class gpio(RegisterHardwareLayer):
 
     def __init__(self, intf, conf):
 
-        io_width = 8
+        self._size = 8
         if 'size' in conf.keys():
-            io_width = conf['size']
+            self._size = conf['size']
 
-        io_bytes = ((io_width - 1) / 8) + 1
+        io_bytes = ((self._size - 1) / 8) + 1
 
         self._registers['INPUT'] = {'descr': {'addr': 1, 'size': io_bytes, 'properties': ['ro', 'byte_array']}}
         self._registers['OUTPUT'] = {'descr': {'addr': 2 + io_bytes - 1, 'size': io_bytes, 'properties': ['byte_array']}}
