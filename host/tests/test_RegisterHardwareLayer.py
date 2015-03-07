0e8dc0296e18bbbab7827bb4a5e4f72d096c0274
Jens Janssen <janssen@physik.uni-bonn.de>
2015-03-07 20:11:09 +0100

    BUG: fix test of HardwareRegisterLayer

--- host/tests/test_RegisterHardwareLayer.py
+++ host/tests/test_RegisterHardwareLayer.py
@@ -142,5 +142,34 @@ class TestRegisterHardwareLayer(unittest.TestCase):
         conf = self.dut.get_configuration()
         self.assertDictEqual({'test_register': {'REG1': 12, 'REG2': 1, 'REG3': 65535, 'REG_test_init': 0, 'REG_byte_array': [1, 2, 3, 4]}, 'dummy_tl': {}}, conf)
 
+    def test_get_set_value(self):
+        for val in range(256):
+            self.dut['test_register'].set_value(val, 0, size=8, offset=0)
+            ret_val = self.dut['test_register'].get_value(0, size=8, offset=0)
+            self.assertEqual(ret_val, val)
+
+    def test_write_read_reg_with_bit_str(self):
+        val = '00110110'  # 54
+        self.dut['test_register'].set_value(val, 0, size=8, offset=0)
+        ret_val = self.dut['test_register'].get_value(0, size=8, offset=0)
+        self.assertEqual(ret_val, int(val, base=2))
+
+    def test_write_read_reg_with_offset(self):
+        for offset in range(32):
+            val = 131
+            self.dut['test_register'].set_value(val, 0, size=8, offset=offset)
+            ret_val = self.dut['test_register'].get_value(0, size=8, offset=offset)
+            self.assertEqual(ret_val, val)
+
+    def test_write_read_reg_with_size(self):
+        for size in range(8, 33):
+            val = 131
+            self.dut['test_register'].set_value(val, 0, size=size, offset=7)
+            ret_val = self.dut['test_register'].get_value(0, size=size, offset=7)
+            self.assertEqual(ret_val, val)
+
+    def test_wrong_size(self):
+        self.assertRaises(ValueError, self.dut['test_register'].set_value, 131, addr=0, size=7, offset=7)
+
 if __name__ == '__main__':
     unittest.main()
