9245d18bc1ca7a1c8a7278409b09b7222221dbde
DavidLP <pohl@physik.uni-bonn.de>
2015-03-13 15:13:15 +0100

    ENH: new VISA transport layer to talk to almost all lab devices with different interfaces

---
+++ host/basil/TL/SiVisa.py
@@ -0,0 +1,40 @@
+#
+# ------------------------------------------------------------
+# Copyright (c) All rights reserved
+# SiLab, Institute of Physics, University of Bonn
+# ------------------------------------------------------------
+#
+import visa
+
+from basil.TL.TransferLayer import TransferLayer
+
+
+class SiVisa(TransferLayer):
+
+    '''Transfer layer for a Virtual Instrument Software Architecture (VISA) provided by pyVisa.
+    Several interfaces are available (GPIB, RS232, USB, Ethernet). To be able to use pyVisa without
+    the proprietary NI-VISA driver a pyVisa backend pyVisa-py is used.
+    GPIB under linux is not supported via pyVisa-py right now and linux-gpib does not
+    compile on modern kernels right now. Thus no GPIB linux support on modern systems.
+    '''
+
+    def __init__(self, conf):
+        super(TransferLayer, self).__init__(conf)
+        self._port = None
+
+    def init(self):
+        '''
+        Initialize the device.
+        Parameters of visa.ResourceManager('@py').open_resource()
+        '''
+        rm = visa.ResourceManager('@py')
+        self._resource = rm.open_resource(**self._init)
+
+    def write(self, data):
+        self._resource.write(data)
+
+    def read(self):
+        self._resource.read()
+
+    def ask(self, data):
+        return self._resource.query(data)
