068f84ef47e0131ea4f05ca401f58b884e50108e
DavidLP <pohl@physik.uni-bonn.de>
2015-03-13 19:13:53 +0100

    MAINT: commends

--- examples/lab_devices/scpi_devices.py
+++ examples/lab_devices/scpi_devices.py
@@ -7,9 +7,9 @@
 
 ''' Example how to use different laboratory devices (multimeter, pulsers, etc.) that understand SCPI.
     The language (SCPI) is independent of the interface (TCP, RS232, GPIB, USB, etc.). The interfaces
-    can be choosen by an appropriate transportation layer (TL). This ca be a VISA TL and
-    a Serial TL at the moment. VISA TL is recommendet since it gives you access to almost 
-    all laboratory devices (>> 90%) over TCP, RS232, USB, GPIB (Windows only).
+    can be choosen by an appropriate transportation layer (TL). This can be a VISA TL and
+    a Serial TL at the moment. VISA TL is recommendet since it gives you access to almost
+    all laboratory devices (> 90%) over TCP, RS232, USB, GPIB (Windows only so far).
     '''
 
 from basil.dut import Dut
@@ -17,13 +17,13 @@ from basil.dut import Dut
 # Talk to a Keithley device via serial port using pySerial
 dut = Dut('keithley2400_pyserial.yaml')
 dut.init()
-dut['Multimeter'].reset()
 print dut['Multimeter'].get_name()
 
 # Talk to a Keithley device via serial port using VISA with Serial interface
 dut = Dut('keithley2400_pyvisa.yaml')
 dut.init()
 print dut['Multimeter'].get_name()
+# Some additional implemented methods for this device
 print dut['Multimeter'].get_voltage()
 dut['Multimeter'].off()
 dut['Multimeter'].set_voltage(3.14159)
