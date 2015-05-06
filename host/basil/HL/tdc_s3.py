e2cf42a967af660b845adce9c978b98aef0824a4
Jens Janssen <janssen@physik.uni-bonn.de>
2015-05-06 18:12:50 +0200

    MAINT: cleanup

--- host/basil/HL/tdc_s3.py
+++ host/basil/HL/tdc_s3.py
@@ -14,7 +14,6 @@ class tdc_s3(RegisterHardwareLayer):
 
     _registers = {'RESET': {'descr': {'addr': 0, 'size': 8, 'properties': ['writeonly']}},
                   'VERSION': {'descr': {'addr': 0, 'size': 8, 'properties': ['ro']}},
-                  'LOST_DATA_COUNTER': {'descr': {'addr': 6, 'size': 8, 'properties': ['ro']}},
                   'ENABLE': {'descr': {'addr': 1, 'size': 1, 'offset': 0}},
                   'ENABLE_EXTERN': {'descr': {'addr': 1, 'size': 1, 'offset': 1}},
                   'EN_ARMING': {'descr': {'addr': 1, 'size': 1, 'offset': 2}},
@@ -23,7 +22,8 @@ class tdc_s3(RegisterHardwareLayer):
                   'EN_NO_WRITE_TRIG_ERR': {'descr': {'addr': 1, 'size': 1, 'offset': 5}},
                   'EN_INVERT_TDC': {'descr': {'addr': 1, 'size': 1, 'offset': 6}},
                   'EN_INVERT_TRIGGER': {'descr': {'addr': 1, 'size': 1, 'offset': 7}},
-                  'EVENT_COUNTER': {'descr': {'addr': 2, 'size': 32, 'properties': ['ro']}}}
+                  'EVENT_COUNTER': {'descr': {'addr': 2, 'size': 32, 'properties': ['ro']}},
+                  'LOST_DATA_COUNTER': {'descr': {'addr': 6, 'size': 8, 'properties': ['ro']}}}
     _require_version = "==2"
 
     def __init__(self, intf, conf):
