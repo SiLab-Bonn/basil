0ed951ca03c5b0c818a6b629774b7028d9b3b5df
Jens Janssen <janssen@physik.uni-bonn.de>
2015-11-27 12:31:44 +0100

    MAINT: adding chip select fpga

--- firmware/modules/utils/fx2_to_bus.v
+++ firmware/modules/utils/fx2_to_bus.v
@@ -10,7 +10,7 @@
 
 module fx2_to_bus
 #(
-    parameter                   WIDTH = 16
+    parameter                   WIDTH = 16 // 16 bit bus from FX2
 ) (
     input wire [WIDTH-1:0]      ADD,
     input wire                  RD_B, // neg active, two clock cycles
@@ -19,20 +19,23 @@ module fx2_to_bus
     input wire                  BUS_CLK, // FCLK
     output wire [WIDTH-1:0]     BUS_ADD,
     output wire                 BUS_RD,
-    output wire                 BUS_WR
+    output wire                 BUS_WR,
+    output wire                 CS_FPGA
 );
 
 // remove offset from FX2
 assign BUS_ADD = ADD - 16'h4000;
 
+// chip select FPGA
+assign CS_FPGA = ~ADD[15] & ADD[14];
+
 // generate read strobe which one clock cycle long
 // this is very important to prevent corrupted data
 reg RD_B_FF;
-always @ (posedge BUS_CLK)
-begin
+always @ (posedge BUS_CLK) begin
     RD_B_FF <= RD_B;
 end
-assign BUS_RD = (~RD_B & RD_B_FF);
+assign BUS_RD = ~RD_B & RD_B_FF;
 
 assign BUS_WR = ~WR_B;
 
