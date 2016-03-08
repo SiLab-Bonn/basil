305636da10f54f43d7b6bf35041f296c747c72ee
Jens Janssen <janssen@physik.uni-bonn.de>
2016-03-08 14:20:15 +0100

    BUG: fix output signals in case of active low, sample signal leading edge with 640MHz

--- firmware/modules/utils/ddr_des.v
+++ firmware/modules/utils/ddr_des.v
@@ -16,20 +16,22 @@ module ddr_des
     input wire CLK, 
     input wire WCLK,
     input wire IN,
-    output reg [CLKDV*4-1:0] OUT
+    output reg [CLKDV*4-1:0] OUT,
+    output wire [1:0] OUT_FAST
 );
 
 wire [1:0] DDRQ;
 IDDR IDDR_inst (
    .Q1(DDRQ[1]), // 1-bit output for positive edge of clock 
    .Q2(DDRQ[0]), // 1-bit output for negative edge of clock
-   .C(CLK2X),   // 1-bit clock input
+   .C(CLK2X), // 1-bit clock input
    .CE(1'b1), // 1-bit clock enable input
-   .D(IN),   // 1-bit DDR data input
-   .R(1'b0),   // 1-bit reset
-   .S(1'b0)    // 1-bit set
+   .D(IN), // 1-bit DDR data input
+   .R(1'b0), // 1-bit reset
+   .S(1'b0) // 1-bit set
 );
 
+assign OUT_FAST = DDRQ;
 
 reg [1:0] DDRQ_DLY;
 
@@ -43,7 +45,7 @@ always@(posedge CLK2X)
  reg [3:0] DDRQ_DATA_BUF;
 always@(posedge CLK2X)
     DDRQ_DATA_BUF[3:0] <= DDRQ_DATA[3:0];
-    
+
 reg [3:0] DATA_IN;
 always@(posedge CLK)
     DATA_IN[3:0] <= DDRQ_DATA_BUF[3:0];
