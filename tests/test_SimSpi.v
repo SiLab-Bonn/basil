7f91deeeedcd955ff88829338616761b6502f86a
Jens Janssen <janssen@physik.uni-bonn.de>
2016-05-19 09:12:40 +0200

    MAINT: cleanup

--- tests/test_SimSpi.v
+++ tests/test_SimSpi.v
@@ -6,7 +6,7 @@
 
 `timescale 1ps / 1ps
 
- 
+
 `include "utils/bus_to_ip.v"
  
 `include "gpio/gpio.v"
@@ -39,9 +39,9 @@ module tb (
     inout wire  [31:0]  BUS_DATA,
     input wire          BUS_RD,
     input wire          BUS_WR,
-    output wire     BUS_BYTE_ACCESS
-);   
-
+    output wire         BUS_BYTE_ACCESS
+);
+    
     // MODULE ADREESSES //
     localparam GPIO_BASEADDR = 32'h0000;
     localparam GPIO_HIGHADDR = 32'h1000-1;
@@ -49,25 +49,25 @@ module tb (
     localparam SPI_BASEADDR = 32'h1000; //0x1000
     localparam SPI_HIGHADDR = 32'h2000-1;   //0x300f
     
-    localparam FAST_SR_AQ_BASEADDR = 32'h2000;                    
-    localparam FAST_SR_AQ_HIGHADDR = 32'h3000-1; 
-
-    localparam PULSE_BASEADDR = 32'h3000;                    
+    localparam FAST_SR_AQ_BASEADDR = 32'h2000;
+    localparam FAST_SR_AQ_HIGHADDR = 32'h3000-1;
+    
+    localparam PULSE_BASEADDR = 32'h3000;
     localparam PULSE_HIGHADDR = PULSE_BASEADDR + 15;
-     
+    
     localparam FIFO_BASEADDR = 32'h8000;
     localparam FIFO_HIGHADDR = 32'h9000-1;
- 
+    
     localparam FIFO_BASEADDR_DATA = 32'h8000_0000;
     localparam FIFO_HIGHADDR_DATA = 32'h9000_0000;
- 
+    
     localparam ABUSWIDTH = 32;
     assign BUS_BYTE_ACCESS = BUS_ADD < 32'h8000_0000 ? 1'b1 : 1'b0;
     
     // MODULES //
-    gpio 
-    #( 
-        .BASEADDR(GPIO_BASEADDR), 
+    gpio
+    #(
+        .BASEADDR(GPIO_BASEADDR),
         .HIGHADDR(GPIO_HIGHADDR),
         .ABUSWIDTH(ABUSWIDTH),
         .IO_WIDTH(8),
@@ -86,8 +86,8 @@ module tb (
     wire SPI_CLK;
     wire EX_START_PULSE;
     pulse_gen
-    #( 
-        .BASEADDR(PULSE_BASEADDR), 
+    #(
+        .BASEADDR(PULSE_BASEADDR),
         .HIGHADDR(PULSE_HIGHADDR),
         .ABUSWIDTH(ABUSWIDTH)
     ) i_pulse_gen
@@ -112,12 +112,12 @@ module tb (
         .CE(),
         .CLOCK(SPI_CLK)
     ); 
-
+    
     wire SCLK, SDI, SDO, SEN, SLD;
     
-    spi 
-    #( 
-        .BASEADDR(SPI_BASEADDR), 
+    spi
+    #(
+        .BASEADDR(SPI_BASEADDR),
         .HIGHADDR(SPI_HIGHADDR),
         .ABUSWIDTH(ABUSWIDTH),
         .MEM_BYTES(16) 
@@ -129,10 +129,10 @@ module tb (
         .BUS_DATA(BUS_DATA[7:0]),
         .BUS_RD(BUS_RD),
         .BUS_WR(BUS_WR),
-    
+        
         .SPI_CLK(SPI_CLK),
         .EXT_START(EX_START_PULSE),
-    
+        
         .SCLK(SCLK),
         .SDI(SDI),
         .SDO(SDO),
@@ -146,9 +146,9 @@ module tb (
     wire FIFO_EMPTY_SPI_RX;
     wire [31:0] FIFO_DATA_SPI_RX;
     
-    fast_spi_rx 
-    #(         
-        .BASEADDR(FAST_SR_AQ_BASEADDR), 
+    fast_spi_rx
+    #(
+        .BASEADDR(FAST_SR_AQ_BASEADDR),
         .HIGHADDR(FAST_SR_AQ_HIGHADDR),
         .ABUSWIDTH(ABUSWIDTH)
     ) i_pixel_sr_fast_rx
@@ -163,12 +163,12 @@ module tb (
         .SCLK(~SPI_CLK),
         .SDI(SDI),
         .SEN(SEN),
-    
+        
         .FIFO_READ(FIFO_READ_SPI_RX),
         .FIFO_EMPTY(FIFO_EMPTY_SPI_RX),
         .FIFO_DATA(FIFO_DATA_SPI_RX)
 
-    ); 
+    );
     
     wire FIFO_READ, FIFO_EMPTY;
     wire [31:0] FIFO_DATA;
@@ -176,7 +176,7 @@ module tb (
     assign FIFO_EMPTY = FIFO_EMPTY_SPI_RX;
     assign FIFO_READ_SPI_RX = FIFO_READ;
     
-    bram_fifo 
+    bram_fifo
     #(
         .BASEADDR(FIFO_BASEADDR),
         .HIGHADDR(FIFO_HIGHADDR),
@@ -190,20 +190,20 @@ module tb (
         .BUS_DATA(BUS_DATA),
         .BUS_RD(BUS_RD),
         .BUS_WR(BUS_WR),
-
+        
         .FIFO_READ_NEXT_OUT(FIFO_READ),
         .FIFO_EMPTY_IN(FIFO_EMPTY),
         .FIFO_DATA(FIFO_DATA),
-
+        
         .FIFO_NOT_EMPTY(),
         .FIFO_FULL(),
         .FIFO_NEAR_FULL(),
         .FIFO_READ_ERROR()
     );
-
+    
     initial begin
         $dumpfile("spi.vcd");
         $dumpvars(0);
     end
-
+    
 endmodule
