9b63b51049dbb868e9f38897c41348012cb3cbb0
Jens Janssen <janssen@physik.uni-bonn.de>
2016-05-19 09:22:10 +0200

    MAINT: cleanup

--- examples/register_example/simple_register_example.py
+++ examples/register_example/simple_register_example.py
@@ -1,3 +1,9 @@
+# ------------------------------------------------------------
+# Copyright (c) All rights reserved
+# SiLab, Institute of Physics, University of Bonn
+# ------------------------------------------------------------
+#
+
 from basil.dut import Dut
 
 chip = Dut("simple_register_example.yaml")
@@ -5,13 +11,12 @@ chip = Dut("simple_register_example.yaml")
 print(chip['TEST'])
 print(chip['REG'])
 
-
-#chip['REG'][0] = 1
+# chip['REG'][0] = 1
 chip['REG']['VPULSE'][5] = 1
-#chip['REG']['VPULSE'] = 1
+# chip['REG']['VPULSE'] = 1
 
-#chip['REG']['COLUMN'][0] = 1 # does not work yet
-#chip['REG'][0] = 1 # does not work yet
+# chip['REG']['COLUMN'][0] = 1 # does not work yet
+# chip['REG'][0] = 1 # does not work yet
 
 chip['REG']['COLUMN'][0]['EnR'] = 1
 chip['REG']['COLUMN'][0]['DACR'] = 3
@@ -23,7 +28,3 @@ chip['REG']['VINJECT'][0] = 1
 print(chip['REG'])
 
 print('VINJECT', str(chip['REG']['VINJECT']))
-
-
-
-
