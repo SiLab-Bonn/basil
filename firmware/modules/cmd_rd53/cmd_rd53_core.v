09937a445cce13567ce266c558407ee25b57133b
MarcoVogt <vogt@physik.uni-bonn.de>
2016-06-27 13:03:16 +0200

    Update cmd_rd53_core.v

--- firmware/modules/cmd_rd53/cmd_rd53_core.v
+++ firmware/modules/cmd_rd53/cmd_rd53_core.v
@@ -19,29 +19,26 @@ module cmd_rd53_core
     output reg [7:0]            BUS_DATA_OUT,
 
     input wire                  CMD_CLK_IN,
+    output reg					CMD_SERIAL_OUT,
     output reg [7:0]			CMD_DATA_OUT
 );
-
 
 localparam VERSION = 1;
 localparam REGSIZE = 16;
 localparam BRAM_ABUSWIDTH = 10;
 localparam CMD_MEM_SIZE = 2**BRAM_ABUSWIDTH;
-localparam SYNC_CYCLES_INITIALSYNC = 8;
-localparam SYNC_CYCLES_RESYNC = 1;
-localparam SYNC_TIMEOUT_PRESET = 32;
-
-localparam SYNC_AUTOMATICALLY = 0;	//1: Interrupt data transmission for sync pattern after timout, defined by SYNC_TIMEOUT_PRESET
+//localparam SYNC_CYCLES_INITIALSYNC = 32;
 
 wire [15:0] SYNC_PATTERN;
 assign SYNC_PATTERN = 16'b1000000101111110;
 
 reg [7:0] sync_cycle_cnt = 8'h00;
-reg [7:0] sync_cycles;
-reg [7:0] sync_timeout_cnt;
 reg [15:0] repeat_cnt = 16'h0000;
 reg data_pending = 1'b0;
 reg SYNCING = 1'b0;
+reg serdes_next_byte = 1'b0;
+reg serializer_active = 1'b1;
+reg [7:0] CMD_DATA_OUT_SR = 8'h00;
 
 // flags
 wire RST;
@@ -64,7 +61,7 @@ wire START_SYNC;
 cdc_pulse_sync start_pulse_sync (.clk_in(BUS_CLK), .pulse_in(START), .clk_out(CMD_CLK_IN), .pulse_out(START_SYNC));
 
 
-wire CONF_EN_EXT_START;
+// wire CONF_EN_EXT_START;
 reg CONF_DONE;
 
 //Registers
@@ -92,15 +89,10 @@ always @(posedge BUS_CLK) begin
         status_regs[BUS_ADD[3:0]] <= BUS_DATA_IN;
 end
 
-//assign CONF_EN_EXT_START = status_regs[1][0];
-//assign CONF_DONE = status_regs[1][1];
-wire SYNC_AUTOMODE;// = 1;
-assign SYNC_AUTOMODE = status_regs[2][0];
 wire [15:0] CONF_CMD_SIZE;// = CMD_MEM_SIZE;     // Reg 3+4
 assign CONF_CMD_SIZE = {status_regs[4], status_regs[3]};
 wire [15:0] CONF_REPEAT_COUNT; // Reg 5+6
 assign CONF_REPEAT_COUNT = {status_regs[6], status_regs[5]};
-
 wire [7:0] BUS_STATUS_OUT;
 assign BUS_STATUS_OUT = status_regs[BUS_ADD[3:0]];
 
@@ -112,7 +104,7 @@ always @ (posedge BUS_CLK) begin
         if(BUS_ADD == 0)
             BUS_DATA_OUT_REG <= VERSION;
         else if(BUS_ADD == 1)
-            BUS_DATA_OUT_REG <= {CONF_EN_EXT_START, 4'b0, SYNC_AUTOMODE, SYNCING, CONF_DONE};
+            BUS_DATA_OUT_REG <= {6'b0, SYNCING, CONF_DONE};
         else if(BUS_ADD == 3)
 	        BUS_DATA_OUT_REG <= CONF_CMD_SIZE[7:0];
         else if(BUS_ADD == 4)
@@ -125,20 +117,22 @@ always @ (posedge BUS_CLK) begin
             BUS_DATA_OUT_REG <= BUS_STATUS_OUT;
         end
     else
-        BUS_DATA_OUT_REG <= 7'h00;
+        BUS_DATA_OUT_REG <= 8'h00;
 end
 
 // wait cycle for bram ???
-reg [ABUSWIDTH-1:0]  PREV_BUS_ADD;
+reg [ABUSWIDTH-1:0] PREV_BUS_ADD = 16'h0000;
 always @ (posedge BUS_CLK) begin
-    if(BUS_RD) begin
+    if(BUS_RD)
         PREV_BUS_ADD <= BUS_ADD;
-    end
+    else
+	    PREV_BUS_ADD <= 16'h0000;
 end
 
 
 // Mux: RAM, registers
 reg [7:0] OUT_MEM;
+reg OUT_SR;
 always @(*) begin
     if(PREV_BUS_ADD < REGSIZE)
         BUS_DATA_OUT = BUS_DATA_OUT_REG;
@@ -152,11 +146,11 @@ end
 reg [7:0] OUT_MEM_CMD_CLK;
 
 //DIFFERENT METHOD
-reg [BRAM_ABUSWIDTH-1:0] read_address_reg, read_address = 0;
+reg [BRAM_ABUSWIDTH-1:0] read_address = 0; //read_address_reg
 
 // BRAM
 wire BUS_MEM_EN;
-wire [ABUSWIDTH-1:0] BUS_MEM_ADD, BUS_MEM_ADD_CMD_CLK;
+wire [ABUSWIDTH-1:0] BUS_MEM_ADD;//, BUS_MEM_ADD_CMD_CLK;
 
 assign BUS_MEM_EN = (BUS_WR | BUS_RD) & BUS_ADD >= REGSIZE;
 assign BUS_MEM_ADD = BUS_ADD[BRAM_ABUSWIDTH-1:0]-REGSIZE;
@@ -175,7 +169,7 @@ always @ (posedge CMD_CLK_IN)
 
 // FSM
 reg START_FSM;
-localparam STATE_INIT  = 0, STATE_IDLE = 1, STATE_SYNC = 2, STATE_DATA_WRITE = 3;//, STATE_STOP = 4;
+localparam STATE_INIT  = 0, STATE_IDLE = 1, STATE_SYNC = 2, STATE_DATA_WRITE = 3;
 
 reg [3:0] state, next_state;
 
@@ -192,14 +186,11 @@ end
 always @(posedge BUS_CLK)
     if(RST)
         CONF_DONE <= 1;
-    else if(SYNC_AUTOMODE==0 && state==STATE_SYNC && next_state!=STATE_SYNC && data_pending==0)
+    else if(data_pending==0 && serdes_next_byte)
         CONF_DONE <= 1;
-    else if(state==STATE_IDLE && next_state==STATE_IDLE)
-        CONF_DONE <= 1;
-    else if(SYNC_AUTOMODE==0 && data_pending==0)
-	    CONF_DONE <= 1;
-    else if(START || state==STATE_DATA_WRITE)
+    else if((START || state==STATE_DATA_WRITE) && serdes_next_byte)
         CONF_DONE <= 0;
+
 
 always @ (posedge CMD_CLK_IN) begin
     if (RST_SYNC)
@@ -208,69 +199,44 @@ always @ (posedge CMD_CLK_IN) begin
         state <= next_state;
 end
 
-// SYNC timout
-always @ (posedge CMD_CLK_IN) begin
-    if(state==STATE_SYNC) begin
-        sync_timeout_cnt <= 8'h00;
-	    SYNCING <= 1; end
-    else begin
-        sync_timeout_cnt <= sync_timeout_cnt + 1;
-	    SYNCING <= 0; end
-end
-
 
 always @ (*) begin
     next_state = state;
     case(state)
         STATE_INIT:
-            if(START_FSM) begin
-                sync_cycles <= SYNC_CYCLES_INITIALSYNC;
-                next_state = STATE_SYNC;
-            end
+            if(START_FSM)
+                next_state = STATE_IDLE;
 
         STATE_IDLE: begin
-            if(sync_timeout_cnt >= SYNC_TIMEOUT_PRESET) begin
-                sync_cycles <= SYNC_CYCLES_RESYNC;
-                next_state = STATE_SYNC;
-                end
-            else
 			if(data_pending)
                 next_state = STATE_DATA_WRITE;
+			else
+				next_state = STATE_SYNC;
         end
 
         STATE_SYNC:
-            if(sync_cycle_cnt >= 2*sync_cycles-1)
-	            if(data_pending)
-		            next_state = STATE_DATA_WRITE;
-	            else
-		            if(SYNC_AUTOMODE)
-		            	next_state = STATE_IDLE;
+            if(data_pending)
+	            next_state = STATE_DATA_WRITE;
 
         STATE_DATA_WRITE:
-            if(repeat_cnt >= CONF_REPEAT_COUNT) begin
-	            if(SYNC_AUTOMODE)
-                	next_state = STATE_IDLE;
-	            else
-		            next_state = STATE_SYNC;
-	        	end
-            else if(sync_timeout_cnt >= SYNC_TIMEOUT_PRESET && 	SYNC_AUTOMATICALLY) begin
-                sync_cycles <= SYNC_CYCLES_RESYNC;
-                next_state = STATE_SYNC;
-                end
+            if(repeat_cnt >= CONF_REPEAT_COUNT)
+	            next_state = STATE_SYNC;
+
     endcase
 end
 
 
 
-//Split SYNC_PATTERN
-reg [7:0] sync_halfpattern;
+//SYNC_PATTERN
+reg [7:0] sync_halfpattern = 8'h00;
 always @ (posedge CMD_CLK_IN) begin
     if(state==STATE_SYNC) begin
         if (sync_cycle_cnt[0])
             sync_halfpattern <= SYNC_PATTERN[15:8];
         else
             sync_halfpattern <= SYNC_PATTERN[7:0];
-        sync_cycle_cnt <= sync_cycle_cnt + 1;
+        if(serdes_next_byte)
+        	sync_cycle_cnt <= sync_cycle_cnt + 1;
         end
     if(state!=STATE_SYNC || next_state!=STATE_SYNC)
         sync_cycle_cnt <= 8'h00;
@@ -279,12 +245,20 @@ end
 
 //MUX
 always @ (posedge CMD_CLK_IN) begin
-    if(state==STATE_SYNC)
-        CMD_DATA_OUT = sync_halfpattern;
-    else if(state==STATE_DATA_WRITE)
-        CMD_DATA_OUT = OUT_MEM_CMD_CLK;
-    else if(state==STATE_IDLE)
-        CMD_DATA_OUT = 8'h00;
+    if(state==STATE_SYNC) begin // && serdes_next_byte
+	    CMD_DATA_OUT = sync_halfpattern;
+        CMD_DATA_OUT_SR = sync_halfpattern;
+	    end
+    else if(state==STATE_DATA_WRITE) begin
+	    CMD_DATA_OUT = mem[read_address];//OUT_MEM_CMD_CLK;
+        CMD_DATA_OUT_SR = mem[read_address];//OUT_MEM_CMD_CLK;
+        CMD_SERIAL_OUT <= OUT_SR;
+	    end
+    else if(state==STATE_IDLE) begin
+	    CMD_DATA_OUT = 8'h00;
+        CMD_DATA_OUT_SR = 8'h00;
+	    CMD_SERIAL_OUT <= OUT_SR;
+	    end
 end
 
 
@@ -292,32 +266,60 @@ end
 always @ (posedge BUS_CLK) begin
 	if(START && CONF_CMD_SIZE)// && next_state!=STATE_IDLE)
     	data_pending <= 1;
-    else if(repeat_cnt >= CONF_REPEAT_COUNT || state==STATE_IDLE && next_state==STATE_IDLE)
+	else if(state==STATE_DATA_WRITE && next_state!=STATE_DATA_WRITE) //
        	data_pending <= 0;
+end
+
+
+always @ (posedge BUS_CLK) begin
+	if(state!=STATE_INIT) //==STATE_IDLE || state==STATE_DATA_WRITE)
+		serializer_active <= 1'b1;
+	else
+		serializer_active <= 1'b0;
+end
+
+
+//SERIALIZER
+reg [7:0] serializer_shift_register;// = 8'h00;
+reg [2:0] serdes_cnt = 3'b000;
+always @ (posedge CMD_CLK_IN) begin
+	if(serializer_active) begin
+		serializer_shift_register <= {1'b0, serializer_shift_register [7:1]};//, 1'b0};
+		if(serdes_cnt == 3'b111) begin
+			serdes_next_byte <= 1;
+			serializer_shift_register <= CMD_DATA_OUT_SR;
+			serdes_cnt <= 3'b000;
+		end
+		else begin
+			serdes_cnt <= serdes_cnt + 1;
+			serdes_next_byte <= 0;
+		end
+
+		OUT_SR <= serializer_shift_register[0];
+	end
+	else
+		serdes_cnt <= 3'b000;
+
 end
 
 
 //DATA_WRITE
 always @ (posedge BUS_CLK) begin
-    if(next_state==STATE_DATA_WRITE) begin
-        if(read_address < CONF_CMD_SIZE-1)
-            read_address <= read_address + 1;
-        else begin
-            read_address <= 10'b0;
-            repeat_cnt <= repeat_cnt + 1;
-        	end
-      	if(repeat_cnt >= CONF_REPEAT_COUNT) begin
-	        repeat_cnt <= 16'h0000;
-        	read_address <= 10'b0;
-      	end
+    if(state==STATE_DATA_WRITE) begin // && serdes_next_byte
+    	if(serdes_next_byte) begin
+	        if(read_address < CONF_CMD_SIZE-1)
+           		read_address <= read_address + 1;
+	        else begin
+	            read_address <= 10'b0;
+		        if(repeat_cnt == CONF_REPEAT_COUNT)
+			        repeat_cnt <= 16'h0000;
+		        else
+			        repeat_cnt <= repeat_cnt + 1;
+	        end
+    	end
+    else
+	    repeat_cnt <= 16'h0000;
     end
-    if(state==STATE_DATA_WRITE) begin
-	    if(repeat_cnt >= CONF_REPEAT_COUNT) begin
-			repeat_cnt <= 16'h0000;
-        	read_address <= 10'b0;
-	    end
-    end
-
 end
 
 endmodule
