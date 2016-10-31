622424ec0ec3ab4b6c700eaa46b3849ed8332739
Tomasz Hemperek <themperek@gmail.com>
2016-10-31 09:08:03 +0100

    ENH: add setting pattern for fadc

--- basil/HL/FadcConf.py
+++ basil/HL/FadcConf.py
@@ -15,7 +15,7 @@ class FadcConf(HardwareLayer):
 
     def init(self):
 
-        logging.debug("Initializing FADC Configuration...")
+        logging.info("Initializing FADC Configuration...")
 
         self._intf.set_data([0x00, 0x10])  # RESET ADC
         self._intf.start()
@@ -31,3 +31,16 @@ class FadcConf(HardwareLayer):
         self._intf.start()
         while not self._intf.is_done():
             pass
+
+    def enable_pattern(self, pattern):
+        
+        self._intf.set_data([0x03, 0x80 | ((pattern & 0x3f00) >> 8 )]) 
+        self._intf.start()
+        while not self._intf.is_done():
+            pass
+
+        self._intf.set_data([0x04, pattern & 0xff]) 
+        self._intf.start()
+        while not self._intf.is_done():
+            pass
+        
