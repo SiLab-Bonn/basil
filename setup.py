7260e4a6651329ac63f2652d3e5fbb09cf57a3c0
Jens Janssen <janssen@physik.uni-bonn.de>
2018-01-11 14:59:24 +0100

    MAINT: build related updates, adding requirements.txt

--- setup.py
+++ setup.py
@@ -1,27 +1,39 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-from setuptools import find_packages
-
-f = open('VERSION', 'r')
-basil_version = f.readline().strip()
-f.close()
+from setuptools import setup, find_packages
 
 author = 'Tomasz Hemperek, Jens Janssen, David-Leon Pohl'
 author_email = 'hemperek@uni-bonn.de, janssen@physik.uni-bonn.de, pohl@physik.uni-bonn.de'
 
+# https://packaging.python.org/guides/single-sourcing-package-version/
+# Use
+#     import get_distribution
+#     get_distribution('package_name').version
+# to programmatically access a version number.
+# Also add
+#     include VERSION
+# MANIFEST.in
+with open('VERSION') as version_file:
+    version = version_file.read().strip()
+
+# Requirements for core functionality from requirements.txt
+# Also add
+#     include requirements.txt
+# MANIFEST.in
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
 setup(
     name='basil_daq',
-    version=basil_version,
+    version=version,
     description='Basil - a data acquisition and system testing framework',
     url='https://github.com/SiLab-Bonn/basil',
     license='BSD 3-Clause ("BSD New" or "BSD Simplified") License',
     long_description='Basil is a modular data acquisition system and system testing framework in Python.\nIt also provides generic FPGA firmware modules for different hardware platforms and drivers for wide range of lab appliances.',
-    requires=['bitarray (>=0.8.1)', 'pyyaml', 'numpy'],
+    install_requires=install_requires,
     author=author,
     maintainer=author,
     author_email=author_email,
     packages=find_packages(),
     include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
+    package_data={'': ['*.yaml']},
     platforms='any'
 )
