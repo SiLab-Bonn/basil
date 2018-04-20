08c904c5387cf88f1eb36a56def46c37a497fe75
Jens Janssen <janssen@physik.uni-bonn.de>
2018-04-20 09:59:15 +0200

    ENH: increase register size, calculate transfer speed and amount, fix LED behavior

--- examples/test_eth/test_eth.py
+++ examples/test_eth/test_eth.py
@@ -1,7 +1,8 @@
 import logging
 import signal
 import time
-from threading import Thread  # , Event, Lock, Condition
+import random
+from threading import Thread, Event  # , Lock, Condition
 
 import numpy as np
 
@@ -45,10 +46,11 @@ class test_eth(RegisterHardwareLayer):
         'RESET': {'descr': {'addr': 0, 'size': 8, 'properties': ['writeonly']}},
         'VERSION': {'descr': {'addr': 0, 'size': 8, 'properties': ['readonly']}},
         'SETUP': {'default': 0, 'descr': {'addr': 1, 'size': 8, 'offset': 0}},
-        'UDP_WRITE_CNT': {'descr': {'addr': 2, 'size': 32, 'offset': 0}},
-        'TCP_WRITE_DLY': {'default': 0, 'descr': {'addr': 6, 'size': 32, 'offset': 0}},
-        'TCP_WRITE_CNT': {'descr': {'addr': 10, 'size': 32, 'offset': 0, 'properties': ['readonly']}},
-        'TCP_FAILED_WRITE_CNT': {'descr': {'addr': 14, 'size': 32, 'offset': 0, 'properties': ['readonly']}}
+        'TEST_DATA': {'descr': {'addr': 2, 'size': 64, 'offset': 0}},
+        'UDP_WRITE_CNT': {'descr': {'addr': 10, 'size': 32, 'offset': 0}},
+        'TCP_WRITE_DLY': {'default': 0, 'descr': {'addr': 14, 'size': 16, 'offset': 0}},
+        'TCP_WRITE_CNT': {'descr': {'addr': 16, 'size': 64, 'offset': 0, 'properties': ['readonly']}},
+        'TCP_FAILED_WRITE_CNT': {'descr': {'addr': 24, 'size': 64, 'offset': 0, 'properties': ['readonly']}}
     }
 
 
@@ -62,35 +64,43 @@ class Test(object):
         signal.signal(signal.SIGINT, self.signal_handler)
         logging.info('Press Ctrl-C to stop')
 
-        self.stop_thread = False
+        self.stop_thread = Event()
         self.total_data_err_cnt = 0
 
     def signal_handler(self, signum, frame):
         logging.info('Pressed Ctrl-C...')
+        self.stop_thread.set()
         signal.signal(signal.SIGINT, signal.SIG_DFL)  # setting default handler
-        self.stop_thread = True
 
     def start(self, test_tcp=True, test_udp=True):
         # reset registers
         self.dut['REGISTERS'].RESET
         # setup register values
-        self.dut['REGISTERS'].TCP_WRITE_DLY = 5  # set delay: 1 equivalent to 1/133MHz=0.0075us=7.5ns
+        self.monitor_delay = 3.0  # Speed of displaying netowrk speed
+        self.tcp_readout_delay = 0.1  # Delay between reading TCP buffer
+        self.dut['REGISTERS'].TCP_WRITE_DLY = 6  # set TCP write delay: 1 equivalent to write data every clock cycle (1/133MHz=0.0075us=7.5ns)
         self.total_data_err_cnt = 0
-        self.tatal_data_words_read = 0
-        self.udp_delay = 0.0
+        self.total_data_words_read = 0
+        self.udp_readout_delay = 0.0  # Delay between reading/writing UDP
         self.total_udp_err_cnt = 0
-        self.tatal_udp_read_write_cnt = 0
+        self.total_udp_read_write_cnt = 0
 
+        self.stop_thread.clear()
+        self.mon_t = Thread(target=self.monitor, name='Monitor thread', kwargs={})
+        self.mon_t.daemon = True
         self.tcp_t = Thread(target=self.tcp_read, name='TCP thread', kwargs={})
         self.tcp_t.daemon = True
         self.udp_t = Thread(target=self.udp_read_write, name='UDP thread', kwargs={})
         self.udp_t.daemon = True
+        self.mon_t.start()
         if test_tcp:
             self.tcp_t.start()
         if test_udp:
             self.udp_t.start()
-        while not self.stop_thread:
-            time.sleep(0.05)
+        while not self.stop_thread.wait(0.05):
+            pass
+        self.tcp_t.join()
+        logging.info("Stopped Monitor thread")
         self.tcp_t.join()
         logging.info("Stopped TCP thread")
         self.udp_t.join()
@@ -98,25 +108,59 @@ class Test(object):
         self.tcp_t = None
         # some statistics
         if test_tcp:
+            logging.info("TCP transfer statistics:")
             logging.info("TCP total data error counter: %d" % self.total_data_err_cnt)
-            logging.info("TCP total data words: expected: %d / read: %d" % (self.dut['REGISTERS'].TCP_WRITE_CNT, self.tatal_data_words_read))
+            logging.info("TCP total data words: expected: %d / read: %d" % (self.dut['REGISTERS'].TCP_WRITE_CNT, self.total_data_words_read))
             logging.info("TCP total data words failed: %d" % self.dut['REGISTERS'].TCP_FAILED_WRITE_CNT)
+            if self.total_data_words_read * 4 / 10.0**6 > 1000000:
+                logging.info("Total amount transmitted: %.2f TB" % (self.total_data_words_read * 4 / 10.0**12))
+            elif self.total_data_words_read * 4 / 10.0**6 > 1000:
+                logging.info("Total amount transmitted: %.2f GB" % (self.total_data_words_read * 4 / 10.0**9))
+            else:
+                logging.info("Total amount transmitted: %.2f MB" % (self.total_data_words_read * 4 / 10.0**6))
+
         if test_udp:
+            logging.info("UDP transfer statistics:")
             logging.info("UDP total data words failed: %d" % self.total_udp_err_cnt)
-            logging.info("UDP total read/write counter: expected: %d / read: %d" % (self.tatal_udp_read_write_cnt, self.dut['REGISTERS'].UDP_WRITE_CNT))
+            logging.info("UDP total read/write counter: expected: %d / read: %d" % (self.total_udp_read_write_cnt, self.dut['REGISTERS'].UDP_WRITE_CNT))
+            if self.total_udp_read_write_cnt * 8 / 10.0**6 > 1000000:
+                logging.info("Total amount transmitted: %.2f TB" % (self.total_udp_read_write_cnt * 8 / 10.0**12))
+            elif self.total_udp_read_write_cnt * 8 / 10.0**6 > 1000:
+                logging.info("Total amount transmitted: %.2f GB" % (self.total_udp_read_write_cnt * 8 / 10.0**9))
+            else:
+                logging.info("Total amount transmitted: %.2f MB" % (self.total_udp_read_write_cnt * 8 / 10.0**6))
         # close DUT
         self.dut.close()
+
+    def monitor(self):
+        logging.info("Started Monitor thread")
+        time_read = time.time()
+        last_total_data_words_read = 0
+        last_total_udp_read_write_cnt = 0
+        while not self.stop_thread.wait(max(0.0, self.monitor_delay - time_read + time.time())):
+            tmp_time_read = time.time()
+            tmp_total_data_words_read = self.total_data_words_read
+            tmp_total_udp_read_write_cnt = self.total_udp_read_write_cnt
+            logging.info("TCP write speed: %0.2f Mbit/s" % ((tmp_total_data_words_read - last_total_data_words_read) * 32 / (tmp_time_read - time_read) / 10**6))
+            logging.info("UDP read/write speed: %0.2f Mbit/s" % ((tmp_total_udp_read_write_cnt - last_total_udp_read_write_cnt) * 64 / (tmp_time_read - time_read) / 10**6))
+            time_read = tmp_time_read
+            last_total_data_words_read = tmp_total_data_words_read
+            last_total_udp_read_write_cnt = tmp_total_udp_read_write_cnt
+
+        logging.info("Stopping Monitor thread...")
 
     def tcp_read(self):
         logging.info("Started TCP thread")
         fifo_data_last_value = -1
-        while True:
+        fifo_was_empty = 0
+        time_read = time.time()
+        while not self.stop_thread.wait(max(0.0, self.tcp_readout_delay - time_read + time.time())) or fifo_was_empty < 1:
             time_read = time.time()
-            if self.stop_thread:
+            if self.stop_thread.is_set():
                 self.dut['REGISTERS'].TCP_WRITE_DLY = 0
             fifo_data = self.dut['SITCP_FIFO'].get_data()
             if fifo_data.shape[0]:
-                self.tatal_data_words_read += fifo_data.shape[0]
+                self.total_data_words_read += fifo_data.shape[0]
                 if fifo_data[0] != fifo_data_last_value + 1:
                     logging.warning("TCP not increased by 1 between readouts")
                     self.total_data_err_cnt += (np.abs(fifo_data[0] - fifo_data_last_value + 1))
@@ -124,30 +168,30 @@ class Test(object):
                 if err_cnt:
                     logging.warning("TCP data not increased by 1: errors=%d" % err_cnt)
                     self.total_data_err_cnt += err_cnt
-                logging.info("TCP data words: %d" % fifo_data.shape[0])
                 fifo_data_last_value = fifo_data[-1]
-            elif self.stop_thread:
-                break
-            time.sleep(max(0.0, 1.0 - time_read + time.time()))
+            elif self.stop_thread.is_set():
+                fifo_was_empty += 1
+            if self.stop_thread.is_set():
+                time.sleep(max(0.0, self.tcp_readout_delay - time_read + time.time()))
         logging.info("Stopping TCP thread...")
 
     def udp_read_write(self):
         logging.info("Started UDP thread")
-        udp_data_last_value = 0
-        while True:
+        time_read = time.time()
+        while not self.stop_thread.wait(max(0.0, self.udp_readout_delay - time_read + time.time())):
             time_read = time.time()
-            if self.stop_thread:
-                self.dut['REGISTERS'].SETUP = 0
-                break
-            self.dut['REGISTERS'].SETUP = udp_data_last_value
-            read_value = self.dut['REGISTERS'].SETUP
-            self.tatal_udp_read_write_cnt += 1
-            if read_value != udp_data_last_value:
-                logging.warning("UDP data not correct: read: %d / expected: %d" % (read_value, udp_data_last_value))
-                self.tota_udp_err_cnt += 1
-            udp_data_last_value = (udp_data_last_value + 1)%256  # set max value
-            time.sleep(max(0.0, self.udp_delay - time_read + time.time()))
+            write_value = random.randint(0, 2**64 - 1)
+            self.dut['REGISTERS'].TEST_DATA = write_value
+            read_value = self.dut['REGISTERS'].TEST_DATA
+            self.total_udp_read_write_cnt += 8
+            if read_value != write_value:
+                logging.warning("UDP data not correct: read: %d / expected: %d" % (read_value, write_value))
+                self.total_udp_err_cnt += 1
+            write_value = (write_value + 1) % 2**64  # set max value
+        self.dut['REGISTERS'].TEST_DATA = 0
+        self.total_udp_read_write_cnt += 8
         logging.info("Stopping UDP thread...")
+
 
 if __name__ == "__main__":
     test = Test()
