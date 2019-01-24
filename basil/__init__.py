bbb5bb43f8986a079efb359636e9cd89ad53fb4f
Michael Daas <daas@physik.uni-bonn.de>
2019-01-24 10:06:43 +0100

    FIX: python 2-> 3 bug

--- basil/__init__.py
+++ basil/__init__.py
@@ -4,11 +4,12 @@ import yaml
 
 # Have OrderedDict
 def dict_representer(dumper, data):
-    return dumper.represent_dict(data.iteritems())
+    return dumper.represent_dict(data.items())
 
 
 def dict_constructor(loader, node):
     return collections.OrderedDict(loader.construct_pairs(node))
+
 
 yaml.add_representer(collections.OrderedDict, dict_representer)
 yaml.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, dict_constructor)
