e2cc2368187df7560198011d51ebef05c9f9eb50
Jens Janssen <janssen@physik.uni-bonn.de>
2019-08-15 14:23:59 +0200

    MAINT: remove lambda in favor of function

--- basil/utils/utils.py
+++ basil/utils/utils.py
@@ -23,12 +23,15 @@ def logging(fn):
     return wrapped
 
 
+def lsbits(b):
+    return (b * 0x0202020202 & 0x010884422010) % 1023
+
+
 def bitvector_to_byte_array(bitvector):
     bsize = len(bitvector)
     size_bytes = int(((bsize - 1) / 8) + 1)
     bs = tobytes(array('B', bitvector.vector))[0:size_bytes]
     bitstream_swap = ''
-    lsbits = lambda b: (b * 0x0202020202 & 0x010884422010) % 1023
     for b in bs:
         bitstream_swap += chr(lsbits(b))
     return array('B', bitstream_swap)
