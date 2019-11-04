e269b445147ac3a45b23f26a0c8be1dc41444ee8
Jens Janssen <janssen@physik.uni-bonn.de>
2019-11-04 13:12:03 +0100

    MAINT: cleanup

--- basil/firmware/modules/utils/generic_fifo.v
+++ basil/firmware/modules/utils/generic_fifo.v
@@ -1,6 +1,6 @@
 /**
  * ------------------------------------------------------------
- * Copyright (c) All rights reserved 
+ * Copyright (c) All rights reserved
  * SiLab, Institute of Physics, University of Bonn
  * ------------------------------------------------------------
  */
@@ -9,12 +9,15 @@
 
 
 module gerneric_fifo (
-    clk, reset, write, read,
-    data_in, 
+    clk,
+    reset,
+    write,
+    read,
+    data_in,
     full,
     empty,
     data_out,
-size
+    size
 );
 
 parameter DATA_SIZE = 32;
@@ -27,12 +30,12 @@ output reg empty;
 
 output reg [DATA_SIZE-1:0] data_out;
 `include "../includes/log2func.v"
- 
+
 reg [DATA_SIZE:0] mem [DEPTH-1:0];
 
 parameter POINTER_SIZE = `CLOG2(DEPTH);
 
-reg [POINTER_SIZE-1:0] rd_ponter, rd_tmp, wr_pointer;
+reg [POINTER_SIZE-1:0] rd_pointer, rd_tmp, wr_pointer;
 output reg [POINTER_SIZE-1:0] size;
 
 wire empty_loc;
@@ -40,22 +43,22 @@ wire empty_loc;
 
 always@(posedge clk) begin
     if(reset)
-        rd_ponter <= 0;
+        rd_pointer <= 0;
     else if(read && !empty) begin
-        if(rd_ponter == DEPTH-1'b1)
-            rd_ponter <= 0;
+        if(rd_pointer == DEPTH-1'b1)
+            rd_pointer <= 0;
         else
-            rd_ponter <= rd_ponter + 1'b1;
+            rd_pointer <= rd_pointer + 1'b1;
     end
 end
 
 always@(*) begin
-    rd_tmp = rd_ponter;
+    rd_tmp = rd_pointer;
     if(read && !empty) begin
-        if(rd_ponter == DEPTH-1'b1)
+        if(rd_pointer == DEPTH-1'b1)
             rd_tmp = 0;
         else
-            rd_tmp = rd_ponter + 1'b1;
+            rd_tmp = rd_pointer + 1'b1;
     end
 end
 
@@ -72,15 +75,15 @@ end
 
 always@(posedge clk)
     if(read && !empty)
-        if(rd_ponter == DEPTH-1'b1)
+        if(rd_pointer == DEPTH-1'b1)
             empty <= (wr_pointer == 0);
         else
-            empty <= (wr_pointer == rd_ponter+1'b1);
+            empty <= (wr_pointer == rd_pointer+1'b1);
     else
         empty <= empty_loc;
 
-assign empty_loc = (wr_pointer == rd_ponter);
-assign full = ((wr_pointer==(DEPTH-1'b1) && rd_ponter==0) || (wr_pointer!=(DEPTH-1'b1) && wr_pointer+1'b1 == rd_ponter));
+assign empty_loc = (wr_pointer == rd_pointer);
+assign full = ((wr_pointer==(DEPTH-1'b1) && rd_pointer==0) || (wr_pointer!=(DEPTH-1'b1) && wr_pointer+1'b1 == rd_pointer));
 
 always@(posedge clk)
     if(write && !full)
@@ -91,10 +94,10 @@ always@(posedge clk)
         data_out <= mem[rd_tmp];
 
 always @ (*) begin
-    if(wr_pointer >= rd_ponter)
-        size = wr_pointer - rd_ponter;
+    if(wr_pointer >= rd_pointer)
+        size = wr_pointer - rd_pointer;
     else
-        size = wr_pointer + (DEPTH-rd_ponter);
+        size = wr_pointer + (DEPTH-rd_pointer);
 end
 
 endmodule
