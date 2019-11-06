1936cd70b4cd283ccb04447b348f600e7602ec74
Jens Janssen <janssen@physik.uni-bonn.de>
2019-11-06 18:39:54 +0100

    MAINT: revert compatibility fix made for Cocotb+Icarus, works now out of the box

--- basil/firmware/modules/utils/bus_to_ip.v
+++ basil/firmware/modules/utils/bus_to_ip.v
@@ -1,13 +1,13 @@
 /**
  * ------------------------------------------------------------
- * Copyright (c) All rights reserved 
+ * Copyright (c) All rights reserved
  * SiLab, Institute of Physics, University of Bonn
  * ------------------------------------------------------------
  */
 `timescale 1ps/1ps
 `default_nettype none
- 
- 
+
+
 module bus_to_ip
 #(
     parameter BASEADDR = 0,
@@ -20,10 +20,10 @@ module bus_to_ip
     input wire BUS_WR,
     input wire [ABUSWIDTH-1:0] BUS_ADD,
     inout wire [DBUSWIDTH-1:0] BUS_DATA,
-    
+
     output wire IP_RD,
     output wire IP_WR,
-    output wire [ABUSWIDTH-1:0] IP_ADD,    
+    output wire [ABUSWIDTH-1:0] IP_ADD,
     output wire [DBUSWIDTH-1:0] IP_DATA_IN,
     input wire [DBUSWIDTH-1:0] IP_DATA_OUT
 );
@@ -37,8 +37,6 @@ assign IP_WR = CS ? BUS_WR: 1'b0;
 
 assign IP_DATA_IN =  BUS_DATA;
 
-//assign BUS_DATA = (CS && BUS_WR) ? {DBUSWIDTH{1'bz}} : (CS ? IP_DATA_OUT : {DBUSWIDTH{1'bz}});
-// This is same as above but for Icarus + cocotb this change is needed, and yes, in one line, bug?
-reg [DBUSWIDTH-1:0] TMP; always@(*) TMP = (CS & BUS_WR) ? {DBUSWIDTH{1'bz}} : (CS ? IP_DATA_OUT : {DBUSWIDTH{1'bz}}); assign BUS_DATA = TMP;
+assign BUS_DATA = (CS && BUS_WR) ? {DBUSWIDTH{1'bz}} : (CS ? IP_DATA_OUT : {DBUSWIDTH{1'bz}});
 
 endmodule
