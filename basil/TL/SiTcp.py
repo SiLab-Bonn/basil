243fbde31b0c956dbcc3cda76007e505c0b3d9ad
Jens Janssen <janssen@physik.uni-bonn.de>
2020-03-12 12:22:31 +0100

    MAINT: dereference the thread object by assigning None, same effect as the deliberate use of del

--- basil/TL/SiTcp.py
+++ basil/TL/SiTcp.py
@@ -463,9 +463,7 @@ class SiTcp(SiTransferLayer):
         super(SiTcp, self).close()
         self._stop = True
         self._tcp_readout_thread.join()
-        # https://stackoverflow.com/questions/60487126/why-does-cpython-garbage-collection-with-threads-not-work
-        del self._tcp_readout_thread
-        del self._tcp_read_buff
+        self._tcp_readout_thread = None
         self._sock_udp.close()
         if self._init['tcp_connection']:
             self._sock_tcp.close()
