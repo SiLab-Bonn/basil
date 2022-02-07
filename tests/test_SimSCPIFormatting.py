83da7d3f43507da059e1a951b45a1bb703cd5f8b
leloup314 <wolf@physik.uni-bonn.de>
2022-02-07 14:08:24 +0100

    ENH: add test for formatting as discussed in #168

---
+++ tests/test_SimSCPIFormatting.py
@@ -0,0 +1,52 @@
+import unittest
+import yaml
+import os
+from basil.dut import Dut
+
+k2410def_yaml = os.path.join(os.path.dirname(__file__), "formatting.yaml")
+
+cnfg_yaml = f'''
+transfer_layer:
+  - name     : Visa
+    type     : Visa
+    init     :
+        resource_name : ASRL1::INSTR
+        read_termination : "\\n"
+        write_termination : "\\r\\n"
+        backend : "{k2410def_yaml}@sim"
+
+hw_drivers:
+  - name      : Sourcemeter
+    type      : scpi
+    interface : Visa
+    init      :
+        device : Keithley 2410
+'''
+
+class TestSimScpi(unittest.TestCase):
+
+    def setUp(self):
+        self.cfg = yaml.safe_load(cnfg_yaml)
+        self.device = Dut(self.cfg)
+        self.device.init()
+
+    def tearDown(self):
+        self.device.close()
+
+    def test_read_voltage(self):
+        voltage = self.device['Sourcemeter'].get_voltage().split(',')[0]
+        self.assertEqual(voltage, '-5.124E-05')
+    
+    def test_read_voltage_formatted(self):
+        # Check that formatting is present
+        self.assertTrue(self.device['Sourcemeter'].has_formatting)
+        # Enable formatting
+        self.device['Sourcemeter'].enable_formatting()
+        voltage = self.device['Sourcemeter'].get_voltage()
+        # Disable formatting
+        self.device['Sourcemeter'].disable_formatting()
+        self.assertEqual(voltage, '-5.124E-05')
+    
+
+if __name__ == '__main__':
+    unittest.main()
