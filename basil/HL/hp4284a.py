db026ce6e1323dbeee0f1ed40632c7e47aebf656
leloup314 <wolf@physik.uni-bonn.de>
2022-03-07 23:34:36 +0100

    ENH: add to lcr meter interface

--- basil/HL/hp4284a.py
+++ basil/HL/hp4284a.py
@@ -13,5 +13,19 @@ class hp4284A(scpi):
     Interface to the Hewlett-Packard Precision LCR-meter with additional functionality
     """
 
+    MEASURE_MODES = ('CPRP', 'ZTD')
+
+    @property
+    def frequency(self):
+        return float(self.get_frequency())
+    
+    @frequency.setter
+    def frequency(self, freq):
+        self.set_frequency(f'{freq}' if freq in ('MIN', 'MAX') else f'{freq}HZ')
+
+    def capacitance(self):
+        if self.get_meas_quantity() != self.MEASURE_MODES[0]:
+            raise ValueError('')
+
     def __init__(self, intf, conf):
         super().__init__(intf, conf)
