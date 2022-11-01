613bdddd05c4e88de3c5d515574597e04480000c
leloup314 <wolf@physik.uni-bonn.de>
2022-11-02 00:41:38 +0100

    ENH: update examples

--- examples/lab_devices/iseg_shq.py
+++ examples/lab_devices/iseg_shq.py
@@ -17,16 +17,26 @@ dut.init()
 dut['SHQ'].channel = 1
 
 # Set voltage ramp speed in V/s
-dut['SHQ'].ramp_speed = 10
+dut['SHQ'].ramp_speed = 2
 
 # Set autostart to True in order to automatically start voltage change when new voltage is set
 dut['SHQ'].autostart = True
 
-# Set voltage to 30 volts
-dut['SHQ'].voltage = 30
+# Set voltage to +-30 volts; requires hardware polarity change
+polarity = dut['SHQ'].polarity
+
+dut['SHQ'].high_voltage = 15 * polarity  # Set high voltage; hv_on() / hv_off() 
+dut['SHQ'].v_lim = 20 * polarity  # Set software-side voltage limit
+dut['SHQ'].voltage = 10 * polarity  # Set voltage
+
+# Disable autostart
+dut['SHQ'].autostart = False
 
 # Read back the voltage that is measured at the output
 print(dut['SHQ'].voltage)
+
+# Read back the software-side voltage limit
+print(dut['SHQ'].v_lim)
 
 # Read back the voltage that is set to be the output
 print(dut['SHQ'].voltage_target)
