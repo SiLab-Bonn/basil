dff203a9864720303268cbe03fce33cbf5dfd3e3
leloup314 <wolf@physik.uni-bonn.de>
2023-04-25 17:58:22 +0200

    ENH: add get read check; FIX: correctly set attribute

--- basil/HL/keithley6517a.py
+++ basil/HL/keithley6517a.py
@@ -4,10 +4,14 @@
 # SiLab, Institute of Physics, University of Bonn
 # ------------------------------------------------------------
 #
-
+import logging
+import time
 from types import MethodType
 from time import sleep
 from basil.HL.scpi import scpi
+
+
+logger = logging.getLogger(__name__)
 
 
 def _write_with_delay(orig_write, delay=0.2):
@@ -30,11 +34,22 @@ class keithley6517A(scpi):
 
         self._setup_for_current_measurement = False
         self._setup_for_voltage_measurement = False
+        self._setup_for_resistance_measurement = False
+        self._prev_read_ts = None
+        self._read_settle_time = 0.5
 
     def init(self):
         super(keithley6517A, self).init()
         # Overwrite TL write to add a delay after each write; Keithley 6517A needs this because otherwise errors occur
         self._intf.write = MethodType(_write_with_delay(self._intf.write), self._intf)
+
+    def get_read(self):
+        if self._prev_read_ts is not None: 
+            elapsed_secs = time.time() - self._prev_read_ts
+            if elapsed_secs < self._read_settle_time:
+                logger.warning(f"Keithley 6517A may need increased settling time (currently {elapsed_secs:.2f} s) in between reads for precise measurements!")
+        self._prev_read_ts = time.time()
+        return super(keithley6517A, self).get_read()
 
     def setup_current_measurement(self, current_range=None, current_limit=None, voltage_range=None, filter=('REP', 10)):
         # See manual p.2-23 ff.
@@ -64,6 +79,9 @@ class keithley6517A(scpi):
         if current_limit is not None:
             self.set_current_limit(current_limit)
 
+        self._setup_for_current_measurement = True
+        self._setup_for_voltage_measurement = False
+        self._setup_for_resistance_measurement = False
 
     def get_current(self):
         if not self._setup_for_current_measurement:
