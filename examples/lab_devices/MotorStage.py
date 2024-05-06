d6ac4ad732dbd4df23fe1e11652ef394a9e21c65
silab <silasmue@outlook.de>
2024-05-06 14:38:32 +0200

    MAINT: update example

--- examples/lab_devices/MotorStage.py
+++ examples/lab_devices/MotorStage.py
@@ -22,23 +22,7 @@ dut["MotorStage"].motor_on(address=1)
 time.sleep(0.1)
 dut["MotorStage"].LL(address=1)
 time.sleep(0.1)
-#####################################
-
-
-def wait_pos(target,precision,address): #waits/prints position until desired precision is reached 
-    print("Moving motore from:",dut["MotorStage"].get_position(address),"to" ,target) #absolute target
-    done=False
-    while done is False:
-        pos=dut["MotorStage"].get_position(address)
-        print("motor at",pos,"moving to",target)
-        if abs(pos-target)<= precision:
-            done=True
-        else:
-            time.sleep(0.5)
-    return pos
-
-
-######################examples
+##################################### examples
 
 #print(dut["MotorStage"].get_channel(address=1),"status") # prints status code
 #time.sleep(2)
@@ -46,11 +30,11 @@ def wait_pos(target,precision,address): #waits/prints position until desired pre
 ########### example  of an absoulute movement using sleep
 
 print(dut["MotorStage"].get_position(address=1),"position before") #print position before movement
-
-dut["MotorStage"].set_position(10000,address=1) #absolute movement
-
-time.sleep(10) #wait for movement
-
+time.sleep(1)
+#dut["MotorStage"].set_position(10000,address=1) #absolute movement
+#dut["MotorStage"].abort(address=1)#abort
+dut["MotorStage"].find_edge(1,address=1) #find edge 0=postive 1=negative
+dut["MotorStage"].wait_FE(address=1)
 print(dut["MotorStage"].get_position(address=1),"position after") #print position after movement
 
 ########### example of an relative movement using wait_pos
@@ -59,7 +43,7 @@ print(dut["MotorStage"].get_position(address=1),"position after") #print positio
 #target = dut["MotorStage"].get_position(address=1)+move_r #wait_pos needs absolute target
 
 #dut["MotorStage"].move_relative(move_r,address=1) #relative movement
-#wait_pos(target,500,1) #wait/print movement
+#dut["MotorStage"].wait_pos(target,500,1) #wait/print movement
 
 ########### abort any movement abruptly:
 #dut["MotorStage"].abort(address=1)
