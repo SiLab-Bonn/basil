38142ba6ecd8987642a3e1f444f222f284f5c90b
silab <silasmue@outlook.de>
2024-05-06 16:07:28 +0200

    fixing code

--- examples/lab_devices/MotorStage.py
+++ examples/lab_devices/MotorStage.py
@@ -14,7 +14,7 @@ from basil.dut import Dut
 dut = Dut('mercury_pyserial.yaml')
 dut.init()
 
-# setup (for c-862) 
+# setup (for c-862)
 # needed if mercury is connected the first time after power up
 # MN Motor=on
 # LL: switch logic active low (hardware)
@@ -25,26 +25,26 @@ dut["MotorStage"].LL(address=1)
 time.sleep(0.1)
 
 # move absolute 1000:
-#dut["MotorStage"].set_position(1000, address=1)
+dut["MotorStage"].set_position(1000, address=1)
 
 # get position:
-#print(dut["MotorStage"].get_position(address=1))
+# print(dut["MotorStage"].get_position(address=1))
 
 # move relative 1000:
-#dut["MotorStage"].move_relative(1000,address=1)
+# dut["MotorStage"].move_relative(1000,address=1)
 
 # finding edge example:
-print(dut["MotorStage"].get_position(address=1), "position before")
-time.sleep(1)
-dut["MotorStage"].find_edge(1, address=1)  # find edge 0=postive 1=negative
-dut["MotorStage"].wait_FE(address=1)
-print(dut["MotorStage"].get_position(address=1), "position after")  # print position after movement
+# print(dut["MotorStage"].get_position(address=1), "position before")
+# time.sleep(1)
+# dut["MotorStage"].find_edge(1, address=1)  # find edge 0=postive 1=negative
+# dut["MotorStage"].wait_FE(address=1)
+# print(dut["MotorStage"].get_position(address=1), "position after")  # print position after movement
 
 # example of an relative movement using wait_pos
-#move_r=20000     
-#target = dut["MotorStage"].get_position(address=1)+move_r 
-#dut["MotorStage"].move_relative(move_r,address=1) 
-#dut["MotorStage"].wait_pos(target,500,1) 
+# move_r=20000
+# target = dut["MotorStage"].get_position(address=1)+move_r
+# dut["MotorStage"].move_relative(move_r,address=1)
+# dut["MotorStage"].wait_pos(target,500,1)
 
 # abort any movement abruptly:
-#dut["MotorStage"].abort(address=1)
+# dut["MotorStage"].abort(address=1)
