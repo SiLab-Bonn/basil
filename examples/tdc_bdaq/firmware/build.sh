d391bbce2dc5eaac751e7e0b6272c9810088e442
Marco Vogt <marco.vogt@cern.ch>
2024-11-11 16:10:26 +0100

    cleanup

--- examples/tdc_bdaq/firmware/build.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-# This script downloads the SiTCP Ethernet IP core, creates a vivado project and starts the synthesis.
-# The bitfile is copied to the `output` directory.
-python fw_utils.py
-cd vivado
-time vivado -mode batch -source run.tcl -notrace
