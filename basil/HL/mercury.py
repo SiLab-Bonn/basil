9d6a5c42789adbea5f4cffd3ec0dc03028a058af
Georgios Giakoustidis <georgios.giakoustidis94@gmail.com>
2025-03-17 14:37:10 +0100

    FIX: Trailing white spaces

--- basil/HL/mercury.py
+++ basil/HL/mercury.py
@@ -16,15 +16,15 @@ class Mercury(HardwareLayer):
     '''Driver for the Physiks Instruments Mercury Controller.
     A protocoll via RS 232 serial port is used with 9600/19200/38400/115200 baud rate. The baud rate
     can be set with dip switches, as well as a hardware address to distinguish several
-    devices on one line. Keep in mind that the address that is set via the DIP switches corresponds 
-    to (board number + 1). Check p.45 of the overall manual below on how to properly set the address 
-    of each controller and what the corresponding board number is. The write termination is CR and 
-    the read ends with '\r', '\n', '\x03'. Therefore '\x03' has to be set a read termination in the 
-    transport layer! And a read termination of 0.1 s should be set! Despite the manual telling SCPI 
-    compatibility, this is not correct for our devices with our firmware. The manual explaining 
+    devices on one line. Keep in mind that the address that is set via the DIP switches corresponds
+    to (board number + 1). Check p.45 of the overall manual below on how to properly set the address
+    of each controller and what the corresponding board number is. The write termination is CR and
+    the read ends with '\r', '\n', '\x03'. Therefore '\x03' has to be set a read termination in the
+    transport layer! And a read termination of 0.1 s should be set! Despite the manual telling SCPI
+    compatibility, this is not correct for our devices with our firmware. The manual explaining
     every "native" command:
     https://twiki.cern.ch/twiki/pub/ILCBDSColl/Phase2Preparations/MercuryNativeCommands_MS176E101.pdf
-    The overall manual: 
+    The overall manual:
     https://www.le.infn.it/~chiodini/allow_listing/pi/Manuals/C-863_Benutzerhandbuch_Kurzversion_MS205Dqu200.pdf
     '''
 
