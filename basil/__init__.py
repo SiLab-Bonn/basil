d4e103ae59f9a95ffdd1748e6905fd73111f4446
rpartzsch <rasmus.partzsch@hotmail.com>
2025-10-23 15:04:20 +0200

    build: moved to pyproject toml

--- basil/__init__.py
+++ basil/__init__.py
@@ -1,24 +1,14 @@
-from importlib.metadata import version, PackageNotFoundError
 import collections
 import yaml
 
-
-__version__ = None  # required for initial installation
-
-try:
-    __version__ = version("basil_daq")
-except PackageNotFoundError:
-    __version__ = "(local)"
-
+__version__ = "3.3.0"
 
 # Have OrderedDict
 def dict_representer(dumper, data):
     return dumper.represent_dict(data.items())
 
-
 def dict_constructor(loader, node):
     return collections.OrderedDict(loader.construct_pairs(node))
-
 
 yaml.add_representer(collections.OrderedDict, dict_representer)
 yaml.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, dict_constructor)
