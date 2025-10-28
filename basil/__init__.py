eb6564b16fa8dda5e4e02f116cf0993ebe9ea221
rpartzsch <rasmus.partzsch@hotmail.com>
2025-10-28 10:51:27 +0100

    sty: whitelines in init functions

--- basil/__init__.py
+++ basil/__init__.py
@@ -3,12 +3,15 @@ import yaml
 
 __version__ = "3.3.0"
 
+
 # Have OrderedDict
 def dict_representer(dumper, data):
     return dumper.represent_dict(data.items())
 
+
 def dict_constructor(loader, node):
     return collections.OrderedDict(loader.construct_pairs(node))
+
 
 yaml.add_representer(collections.OrderedDict, dict_representer)
 yaml.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, dict_constructor)
