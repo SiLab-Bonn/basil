0e3fc9d5787cc6cdec97a5385a4249219d66d83b
cbespin <cbespin@uni-bonn.de>
2026-01-30 14:42:32 +0100

    enh: fix pointer size for BRAM FIFO

--- basil/firmware/modules/utils/generic_fifo.v
+++ basil/firmware/modules/utils/generic_fifo.v
@@ -33,7 +33,7 @@ output reg [DATA_SIZE-1:0] data_out;
 
 reg [DATA_SIZE:0] mem [DEPTH-1:0];
 
-localparam POINTER_SIZE = 16;
+localparam POINTER_SIZE = 32;
 
 reg [POINTER_SIZE-1:0] rd_pointer, rd_tmp, wr_pointer;
 output reg [POINTER_SIZE-1:0] size;
