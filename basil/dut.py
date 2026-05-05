43d5fe0be1324dd392f83360264db744af7578aa
kcaisley <kcaisley@uni-bonn.de>
2026-05-05 15:40:10 +0200

    fix: Change alignment of INFO statements in logging

--- basil/dut.py
+++ basil/dut.py
@@ -20,7 +20,7 @@ from six import string_types
 from yaml import safe_load
 
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s"
+    level=logging.INFO, format="%(asctime)s - %(name)s - [%(levelname)s] (%(threadName)-10s) %(message)s"
 )
 
 
